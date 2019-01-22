70d4ae58ebc307730b992c1aef1b4de472328e2e
Johannes Pietrzyk <johannes.pietrzyk@tu-dresden.de>
2019-01-22 22:09:26 +0100

    Added operator new, new[], delete, delete[]
    Replacements of:
    void * operator new( size_t )
    void* operator new[]( size_t )
    void operator delete( void * )
    void operator delete( void * )

--- include/core/memory/mm_glob.h
+++ include/core/memory/mm_glob.h
@@ -26,25 +26,62 @@ extern "C" {
       }
       return morphstore::memory::query_memory_manager::get_instance( ).allocate( p_AllocSize );
    }
+   void free( void *p_FreePtr ) __THROW {
+      if ( morphstore::memory::stdlib_free_ptr == nullptr ) {
+         init_mem_hooks( );
+      }
+      morphstore::memory::query_memory_manager::get_instance( ).deallocate( p_FreePtr );
+   }
 #else
    void * debug_malloc( size_t p_AllocSize, const char *file, int line, const char *func ) __THROW {
-      fprintf( stderr, "%s - Line %d ( %s ): MM Malloc( %zu Byte )\n", file, line, func, p_AllocSize );
+      fprintf( stderr, "[DEBUG]: %s - Line %d ( %s ): MM Malloc( %zu Byte )\n", file, line, func, p_AllocSize );
       if ( morphstore::memory::stdlib_malloc_ptr == nullptr ) {
          init_mem_hooks( );
       }
       return morphstore::memory::query_memory_manager::get_instance( ).allocate( p_AllocSize );
    }
-#  define malloc( X ) debug_malloc( X, __FILE__, __LINE__, __FUNCTION__ )
-#endif
-
-   void free( void *pFreePtr ) __THROW {
+   void debug_free( void *p_FreePtr, const char *file, int line, const char *func ) __THROW {
+      fprintf( stderr, "[DEBUG]: %s - Line %d ( %s ): MM Free( %p )\n", file, line, func, p_FreePtr );
       if ( morphstore::memory::stdlib_free_ptr == nullptr ) {
          init_mem_hooks( );
       }
-      morphstore::memory::query_memory_manager::get_instance( ).deallocate( pFreePtr );
+      morphstore::memory::query_memory_manager::get_instance( ).deallocate( p_FreePtr );
    }
+#  define malloc( X ) debug_malloc( X, __FILE__, __LINE__, __FUNCTION__ )
+#  define free( X ) debug_free( X, __FILE__, __LINE__, __FUNCTION__ )
+#endif
+
+
+
 }
 
 
+void * operator new( size_t p_AllocSize ) {
+   return malloc( p_AllocSize );
+}
+
+void* operator new[]( size_t p_AllocSize ) {
+   return malloc( p_AllocSize );
+}
+
+void operator delete( void * p_FreePtr ) noexcept {
+   free( p_FreePtr );
+}
+void operator delete( void * p_FreePtr, PPUNUSED size_t p_DeallocSize ) noexcept {
+   /**
+    * The standard library implementations of the size-aware deallocation functions (5-8) directly call the
+    * corresponding size-unaware deallocation functions (1-4).
+    * https://en.cppreference.com/w/cpp/memory/new/operator_delete#
+    */
+   free( p_FreePtr );
+}
+
+void operator delete[]( void* p_FreePtr ) noexcept {
+   free( p_FreePtr );
+}
+
+void operator delete[]( void* p_FreePtr, PPUNUSED size_t p_DeallocSize ) noexcept {
+   free( p_FreePtr );
+}
 
 #endif //MORPHSTORE_CORE_MEMORY_MM_GLOB_H
--- test/core/memory/memory_test.cpp
+++ test/core/memory/memory_test.cpp
@@ -9,10 +9,20 @@
 #include "../../../include/core/memory/mm_glob.h"
 
 #include <cstdio>
+#include <vector>
+#include <iostream>
+
 int main( void ) {
-   for( int i = 0; i < 53; ++i ) {
+/*   for( int i = 0; i < 53; ++i ) {
       void * result = malloc( 1024 );
       fprintf(stderr, "Main: Allocated: %p\n", result );
+   }*/
+   std::vector< int > test;
+   for( int i = 0; i < 100; ++i ) {
+      test.push_back( i );
+   }
+   for( int i = 0; i < 100; ++i ) {
+      std::cout << test[ i ] << "\n";
    }
    return 0;
 }
