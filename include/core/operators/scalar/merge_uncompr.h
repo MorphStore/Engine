2dc8e402abbbe2326081e57ab0658754f65d3a16
Patrick Damme <patrick.damme@tu-dresden.de>
2019-02-25 20:26:39 +0100

    Added a new query-operator: Merge.
    - Merges two sorted and unique (position) columns, i.e., calculates their union.
    - Useful for OR-predicates: the two position columns obtained from two select-operators must be merged.
    - Commit contains
      - the general template-based interface
      - a simple scalar reference implementation for uncompressed data, not tuned for efficiency
      - a small test

---
+++ include/core/operators/scalar/merge_uncompr.h
@@ -0,0 +1,102 @@
+/**********************************************************************************************
+ * Copyright (C) 2019 by MorphStore-Team                                                      *
+ *                                                                                            *
+ * This file is part of MorphStore - a compression aware vectorized column store.             *
+ *                                                                                            *
+ * This program is free software: you can redistribute it and/or modify it under the          *
+ * terms of the GNU General Public License as published by the Free Software Foundation,      *
+ * either version 3 of the License, or (at your option) any later version.                    *
+ *                                                                                            *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;  *
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  *
+ * See the GNU General Public License for more details.                                       *
+ *                                                                                            *
+ * You should have received a copy of the GNU General Public License along with this program. *
+ * If not, see <http://www.gnu.org/licenses/>.                                                *
+ **********************************************************************************************/
+
+/**
+ * @file merge_uncompr.h
+ * @brief Template specialization of the merge-operator for uncompressed inputs
+ * and outputs using the scalar processing style. Note that these are simple
+ * reference implementations not tailored for efficiency.
+ * @todo TODOS?
+ */
+
+#ifndef MORPHSTORE_CORE_OPERATORS_SCALAR_MERGE_UNCOMPR_H
+#define MORPHSTORE_CORE_OPERATORS_SCALAR_MERGE_UNCOMPR_H
+
+#include <core/operators/interfaces/merge.h>
+#include <core/morphing/format.h>
+#include <core/storage/column.h>
+#include <core/utils/basic_types.h>
+#include <core/utils/processing_style.h>
+
+#include <cstdint>
+
+namespace morphstore {
+    
+template<>
+const column<uncompr_f> *
+merge_sorted<processing_style_t::scalar>(
+        const column<uncompr_f> * const inPosLCol,
+        const column<uncompr_f> * const inPosRCol,
+        const size_t outPosCountEstimate
+) {
+    const uint64_t * inPosL = inPosLCol->get_data();
+    const uint64_t * inPosR = inPosRCol->get_data();
+    const uint64_t * const inPosLEnd = inPosL + inPosLCol->get_count_values();
+    const uint64_t * const inPosREnd = inPosR + inPosRCol->get_count_values();
+    
+    // If no estimate is provided: Pessimistic allocation size (for
+    // uncompressed data), reached only if the two input columns are disjoint.
+    auto outPosCol = new column<uncompr_f>(
+            bool(outPosCountEstimate)
+            // use given estimate
+            ? (outPosCountEstimate * sizeof(uint64_t))
+            // use pessimistic estimate
+            : (
+                    inPosLCol->get_size_used_byte() +
+                    inPosRCol->get_size_used_byte()
+            )
+    );
+    uint64_t * outPos = outPosCol->get_data();
+    const uint64_t * const initOutPos = outPos;
+    
+    while(inPosL < inPosLEnd && inPosR < inPosREnd) {
+        if(*inPosL < *inPosR) {
+            *outPos = *inPosL;
+            inPosL++;
+        }
+        else if(*inPosR < *inPosL) {
+            *outPos = *inPosR;
+            inPosR++;
+        }
+        else { // *inPosL == *inPosR
+            *outPos = *inPosL;
+            inPosL++;
+            inPosR++;
+        }
+        outPos++;
+    }
+    // At this point, at least one of the operands has been fully consumed and
+    // the other one might still contain data elements, which must be output.
+    while(inPosL < inPosLEnd) {
+        *outPos = *inPosL;
+        outPos++;
+        inPosL++;
+    }
+    while(inPosR < inPosREnd) {
+        *outPos = *inPosR;
+        outPos++;
+        inPosR++;
+    }
+    
+    const size_t outPosCount = outPos - initOutPos;
+    outPosCol->set_meta_data(outPosCount, outPosCount * sizeof(uint64_t));
+    
+    return outPosCol;
+}
+
+}
+#endif //MORPHSTORE_CORE_OPERATORS_SCALAR_MERGE_UNCOMPR_H
---
+++ test/core/operators/merge_test.cpp
@@ -0,0 +1,50 @@
+/**********************************************************************************************
+ * Copyright (C) 2019 by MorphStore-Team                                                      *
+ *                                                                                            *
+ * This file is part of MorphStore - a compression aware vectorized column store.             *
+ *                                                                                            *
+ * This program is free software: you can redistribute it and/or modify it under the          *
+ * terms of the GNU General Public License as published by the Free Software Foundation,      *
+ * either version 3 of the License, or (at your option) any later version.                    *
+ *                                                                                            *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;  *
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  *
+ * See the GNU General Public License for more details.                                       *
+ *                                                                                            *
+ * You should have received a copy of the GNU General Public License along with this program. *
+ * If not, see <http://www.gnu.org/licenses/>.                                                *
+ **********************************************************************************************/
+
+/**
+ * @file merge_test.cpp
+ * @brief A little test/reference of the merge-operator.
+ * @todo TODOS?
+ */
+
+// This must be included first to allow compilation.
+#include <core/memory/mm_glob.h>
+
+#include "operator_test_frames.h"
+#include <core/morphing/format.h>
+#include <core/operators/scalar/merge_uncompr.h>
+#include <core/storage/column.h>
+#include <core/storage/column_gen.h>
+#include <core/utils/processing_style.h>
+
+using namespace morphstore;
+
+int main(void) {
+    const bool allGood = test_op_2in_1out_1val(
+            "Merge",
+            &merge_sorted<processing_style_t::scalar, uncompr_f>,
+            make_column({1, 4, 5, 8, 9, 12}),
+            make_column({1, 6, 8, 12, 15}),
+            "inPosLCol",
+            "inPosRCol",
+            make_column({1, 4, 5, 6, 8, 9, 12, 15}),
+            "outPosCol",
+            0 // use pessimistic output size estimation
+    );
+    
+    return !allGood;
+}
