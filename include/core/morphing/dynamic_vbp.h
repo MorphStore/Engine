6dec8b2cda229a971e6104f623d63b004afe5efd
Patrick Damme <patrick.damme@tu-dresden.de>
2019-04-10 16:29:13 +0200

    Added a new compressed format: dynamic_vbp_f, which is a generalization of SIMD-BP128.
    - Format struct.
    - Morph operators for the (de)compression.
    - Test case.

---
+++ include/core/morphing/dynamic_vbp.h
@@ -0,0 +1,208 @@
+/**********************************************************************************************
+ * Copyright (C) 2019 by MorphStore-Team                                                      *
+ *                                                                                            *
+ * This file is part of MorphStore - a compression aware vectorized column store.             *
+ *                                                                                            *
+ * This program is free software: you can redistribute it and/or modify it under the          *
+ * terms of the GNU General Public License as published by the Free Software Foundation,      *
+ * either version 3 of the License, or (at your option) any later version.                    *
+ *                                                                                            *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;  *
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  *
+ * See the GNU General Public License for more details.                                       *
+ *                                                                                            *
+ * You should have received a copy of the GNU General Public License along with this program. *
+ * If not, see <http://www.gnu.org/licenses/>.                                                *
+ **********************************************************************************************/
+
+/**
+ * @file dynamic_vbp.h
+ * @brief Format struct and (de)compression morph operators for a
+ * generalization of the compressed format of SIMD-BP128.
+ */
+
+#ifndef MORPHSTORE_CORE_MORPHING_DYNAMIC_VBP_H
+#define MORPHSTORE_CORE_MORPHING_DYNAMIC_VBP_H
+
+#include <core/morphing/format.h>
+#include <core/morphing/vbp_routines.h>
+#include <core/storage/column.h>
+#include <core/utils/basic_types.h>
+#include <core/utils/math.h>
+
+#include <cstdint>
+#include <immintrin.h>
+#include <limits>
+#include <stdexcept>
+#include <string>
+
+namespace morphstore {
+    
+    /**
+     * @brief A generalization of the compressed format of SIMD-BP128.
+     * 
+     * This compressed format divides the uncompressed data into blocks of a
+     * fixed-size (in terms of data elements). For each block, all data
+     * elements in the block are represented in the vertical bit-packed layout
+     * with the number of bits required for the largest value in the block,
+     * i.e., each block has its individual bit width. These bit widths
+     * (meta data) are stored at the page-level, whereby each page consists of
+     * a fixed number of blocks.
+     * 
+     * There are two template parameters controlling the concrete layout:
+     * - `t_BlockSize64` The number of uncompressed (64-bit) data elements per
+     *   block
+     * - `t_PageSizeBlocks` The number of blocks per page.
+     * 
+     * In fact, for a block size of 128 data elements and 16 blocks per page,
+     * this is the format called SIMD-BP128 in the literature, with the 
+     * difference that we use 64-bit instead of 32-bit date elements.
+     */
+    template<size_t t_BlockSize64, size_t t_PageSizeBlocks>
+    struct dynamic_vbp_f : public format {
+        static const size_t m_PageSize64 = t_PageSizeBlocks * t_BlockSize64;
+        static const size_t m_MetaSize8 = t_PageSizeBlocks * sizeof(uint8_t);
+        
+        template<typename t_vec_t>
+        static void check_compatibility() {
+            if(t_BlockSize64 % (sizeof(t_vec_t) * bitsPerByte))
+                throw std::runtime_error(
+                        "dynamic_vbp_f: the number of data elements per block "
+                        "must be a multiple of the number of bits per vector "
+                        "register"
+                );
+            if(t_PageSizeBlocks % sizeof(t_vec_t))
+                throw std::runtime_error(
+                    "dynamic_vbp_f: the number of blocks per page must be a "
+                    "multiple of the number of bytes per vector register"
+                );
+        }
+        
+        static void check_count(size_t p_Count) {
+            // @todo Support arbitrary numbers of data elements.
+            if(p_Count % m_PageSize64)
+                throw std::runtime_error(
+                    "dynamic_vbp_f: the number of data elements must be a "
+                    "multiple of the page size in data elements"
+                );
+        }
+    };
+    
+    template<size_t t_BlockSize64, size_t t_PageSizeBlocks>
+    void morph(
+        const column<uncompr_f> * inCol,
+        column<dynamic_vbp_f<t_BlockSize64, t_PageSizeBlocks> > * outCol
+    ) {
+        using my_dynamic_vbp_f = dynamic_vbp_f<t_BlockSize64, t_PageSizeBlocks>;
+        my_dynamic_vbp_f::template check_compatibility<__m128i>();
+        
+        const size_t inCount64 = inCol->get_count_values();
+        my_dynamic_vbp_f::check_count(inCount64);
+        const size_t inCount128 = convert_size<uint64_t, __m128i>(inCount64);
+        
+        const __m128i * in128 = inCol->get_data();
+        const __m128i * const endIn128 = in128 + inCount128;
+        
+        __m128i * out128 = outCol->get_data();
+        const __m128i * const initOut128 = out128;
+        
+        // Iterate over all input pages.
+        while(in128 < endIn128) {
+            uint8_t * const outMeta8 = reinterpret_cast<uint8_t *>(out128);
+            out128 += convert_size<uint8_t, __m128i>(
+                    my_dynamic_vbp_f::m_MetaSize8
+            );
+            // Iterate over all blocks in the current input page.
+            for(
+                    unsigned blockIdx = 0;
+                    blockIdx < t_PageSizeBlocks;
+                    blockIdx++
+            ) {
+                // Determine maximum bit width.
+                __m128i pseudoMax128 = _mm_setzero_si128();
+                for(
+                        unsigned vecIdx = 0;
+                        vecIdx < convert_size<uint64_t, __m128i>(t_BlockSize64);
+                        vecIdx++
+                )
+                    pseudoMax128 = _mm_or_si128(
+                            pseudoMax128,
+                            _mm_load_si128(in128 + vecIdx)
+                    );
+                // @todo Why does the first alternative not work?
+#if 0
+                const uint64_t pseudoMax64 = 1
+                        | _mm_extract_epi64(pseudoMax128, 0)
+                        | _mm_extract_epi64(pseudoMax128, 1);
+#else
+                uint64_t tmp[2];
+                _mm_store_si128(
+                        reinterpret_cast<__m128i *>(&tmp),
+                        pseudoMax128
+                );
+                const uint64_t pseudoMax64 = 1 | tmp[0] | tmp[1];
+#endif
+                const unsigned bw = std::numeric_limits<uint64_t>::digits -
+                        __builtin_clzll(pseudoMax64);
+                
+                // Store the bit width to the meta data.
+                outMeta8[blockIdx] = static_cast<uint8_t>(bw);
+                
+                // Pack the data with that bit width.
+                pack_switch(
+                        bw,
+                        in128,
+                        convert_size<uint64_t, __m128i>(t_BlockSize64),
+                        out128
+                );
+            }
+        }
+        
+        outCol->set_meta_data(
+                inCount64,
+                convert_size<__m128i, uint8_t>(out128 - initOut128)
+        );
+    }
+    
+    template<size_t t_BlockSize64, size_t t_PageSizeBlocks>
+    void morph(
+        const column<dynamic_vbp_f<t_BlockSize64, t_PageSizeBlocks> > * inCol,
+        column<uncompr_f> * outCol
+    ) {
+        using my_dynamic_vbp_f = dynamic_vbp_f<t_BlockSize64, t_PageSizeBlocks>;
+        my_dynamic_vbp_f::template check_compatibility<__m128i>();
+        
+        const size_t inCount64 = inCol->get_count_values();
+        const size_t inCount128 = convert_size<uint64_t, __m128i>(inCount64);
+        
+        const __m128i * in128 = inCol->get_data();
+        const __m128i * const endIn128 = in128 + inCount128;
+        
+        __m128i * out128 = outCol->get_data();
+        
+        // Iterate over all pages in the input.
+        while(in128 < endIn128) {
+            const uint8_t * const inMeta8 = reinterpret_cast<const uint8_t *>(
+                in128
+            );
+            in128 += convert_size<uint8_t, __m128i>(
+                    my_dynamic_vbp_f::m_MetaSize8
+            );
+            // Iterate over all blocks in the current input page.
+            for(unsigned blockIdx = 0; blockIdx < t_PageSizeBlocks; blockIdx++)
+                unpack_switch(
+                        inMeta8[blockIdx],
+                        in128,
+                        out128,
+                        convert_size<uint64_t, __m128i>(t_BlockSize64)
+                );
+        }
+        
+        outCol->set_meta_data(
+                inCount64,
+                convert_size<uint64_t, uint8_t>(inCount64)
+        );
+    }
+    
+}
+#endif //MORPHSTORE_CORE_MORPHING_DYNAMIC_VBP_H
--- include/core/morphing/vbp_routines.h
+++ include/core/morphing/vbp_routines.h
@@ -143,6 +143,160 @@ namespace morphstore {
 #endif
     }
     
+    inline void pack_switch(
+            unsigned bitwidth,
+            const __m128i * & in128,
+            size_t inCount128,
+            __m128i * & out128
+    ) {
+        switch(bitwidth) {
+            // Generated with Python:
+            // for bw in range(1, 64+1):
+            //   print("case {: >2}: pack<{: >2}>(in128, inCount128, out128); break;".format(bw, bw))
+            case  1: pack< 1>(in128, inCount128, out128); break;
+            case  2: pack< 2>(in128, inCount128, out128); break;
+            case  3: pack< 3>(in128, inCount128, out128); break;
+            case  4: pack< 4>(in128, inCount128, out128); break;
+            case  5: pack< 5>(in128, inCount128, out128); break;
+            case  6: pack< 6>(in128, inCount128, out128); break;
+            case  7: pack< 7>(in128, inCount128, out128); break;
+            case  8: pack< 8>(in128, inCount128, out128); break;
+            case  9: pack< 9>(in128, inCount128, out128); break;
+            case 10: pack<10>(in128, inCount128, out128); break;
+            case 11: pack<11>(in128, inCount128, out128); break;
+            case 12: pack<12>(in128, inCount128, out128); break;
+            case 13: pack<13>(in128, inCount128, out128); break;
+            case 14: pack<14>(in128, inCount128, out128); break;
+            case 15: pack<15>(in128, inCount128, out128); break;
+            case 16: pack<16>(in128, inCount128, out128); break;
+            case 17: pack<17>(in128, inCount128, out128); break;
+            case 18: pack<18>(in128, inCount128, out128); break;
+            case 19: pack<19>(in128, inCount128, out128); break;
+            case 20: pack<20>(in128, inCount128, out128); break;
+            case 21: pack<21>(in128, inCount128, out128); break;
+            case 22: pack<22>(in128, inCount128, out128); break;
+            case 23: pack<23>(in128, inCount128, out128); break;
+            case 24: pack<24>(in128, inCount128, out128); break;
+            case 25: pack<25>(in128, inCount128, out128); break;
+            case 26: pack<26>(in128, inCount128, out128); break;
+            case 27: pack<27>(in128, inCount128, out128); break;
+            case 28: pack<28>(in128, inCount128, out128); break;
+            case 29: pack<29>(in128, inCount128, out128); break;
+            case 30: pack<30>(in128, inCount128, out128); break;
+            case 31: pack<31>(in128, inCount128, out128); break;
+            case 32: pack<32>(in128, inCount128, out128); break;
+            case 33: pack<33>(in128, inCount128, out128); break;
+            case 34: pack<34>(in128, inCount128, out128); break;
+            case 35: pack<35>(in128, inCount128, out128); break;
+            case 36: pack<36>(in128, inCount128, out128); break;
+            case 37: pack<37>(in128, inCount128, out128); break;
+            case 38: pack<38>(in128, inCount128, out128); break;
+            case 39: pack<39>(in128, inCount128, out128); break;
+            case 40: pack<40>(in128, inCount128, out128); break;
+            case 41: pack<41>(in128, inCount128, out128); break;
+            case 42: pack<42>(in128, inCount128, out128); break;
+            case 43: pack<43>(in128, inCount128, out128); break;
+            case 44: pack<44>(in128, inCount128, out128); break;
+            case 45: pack<45>(in128, inCount128, out128); break;
+            case 46: pack<46>(in128, inCount128, out128); break;
+            case 47: pack<47>(in128, inCount128, out128); break;
+            case 48: pack<48>(in128, inCount128, out128); break;
+            case 49: pack<49>(in128, inCount128, out128); break;
+            case 50: pack<50>(in128, inCount128, out128); break;
+            case 51: pack<51>(in128, inCount128, out128); break;
+            case 52: pack<52>(in128, inCount128, out128); break;
+            case 53: pack<53>(in128, inCount128, out128); break;
+            case 54: pack<54>(in128, inCount128, out128); break;
+            case 55: pack<55>(in128, inCount128, out128); break;
+            case 56: pack<56>(in128, inCount128, out128); break;
+            case 57: pack<57>(in128, inCount128, out128); break;
+            case 58: pack<58>(in128, inCount128, out128); break;
+            case 59: pack<59>(in128, inCount128, out128); break;
+            case 60: pack<60>(in128, inCount128, out128); break;
+            case 61: pack<61>(in128, inCount128, out128); break;
+            case 62: pack<62>(in128, inCount128, out128); break;
+            case 63: pack<63>(in128, inCount128, out128); break;
+            case 64: pack<64>(in128, inCount128, out128); break;
+        }
+    }
+    
+    inline void unpack_switch(
+            unsigned bitwidth,
+            const __m128i * & in128,
+            __m128i * & out128,
+            size_t outCount128
+    ) {
+        switch(bitwidth) {
+            // Generated with Python:
+            // for bw in range(1, 64+1):
+            //   print("case {: >2}: unpack<{: >2}>(in128, out128, outCount128); break;".format(bw, bw))
+            case  1: unpack< 1>(in128, out128, outCount128); break;
+            case  2: unpack< 2>(in128, out128, outCount128); break;
+            case  3: unpack< 3>(in128, out128, outCount128); break;
+            case  4: unpack< 4>(in128, out128, outCount128); break;
+            case  5: unpack< 5>(in128, out128, outCount128); break;
+            case  6: unpack< 6>(in128, out128, outCount128); break;
+            case  7: unpack< 7>(in128, out128, outCount128); break;
+            case  8: unpack< 8>(in128, out128, outCount128); break;
+            case  9: unpack< 9>(in128, out128, outCount128); break;
+            case 10: unpack<10>(in128, out128, outCount128); break;
+            case 11: unpack<11>(in128, out128, outCount128); break;
+            case 12: unpack<12>(in128, out128, outCount128); break;
+            case 13: unpack<13>(in128, out128, outCount128); break;
+            case 14: unpack<14>(in128, out128, outCount128); break;
+            case 15: unpack<15>(in128, out128, outCount128); break;
+            case 16: unpack<16>(in128, out128, outCount128); break;
+            case 17: unpack<17>(in128, out128, outCount128); break;
+            case 18: unpack<18>(in128, out128, outCount128); break;
+            case 19: unpack<19>(in128, out128, outCount128); break;
+            case 20: unpack<20>(in128, out128, outCount128); break;
+            case 21: unpack<21>(in128, out128, outCount128); break;
+            case 22: unpack<22>(in128, out128, outCount128); break;
+            case 23: unpack<23>(in128, out128, outCount128); break;
+            case 24: unpack<24>(in128, out128, outCount128); break;
+            case 25: unpack<25>(in128, out128, outCount128); break;
+            case 26: unpack<26>(in128, out128, outCount128); break;
+            case 27: unpack<27>(in128, out128, outCount128); break;
+            case 28: unpack<28>(in128, out128, outCount128); break;
+            case 29: unpack<29>(in128, out128, outCount128); break;
+            case 30: unpack<30>(in128, out128, outCount128); break;
+            case 31: unpack<31>(in128, out128, outCount128); break;
+            case 32: unpack<32>(in128, out128, outCount128); break;
+            case 33: unpack<33>(in128, out128, outCount128); break;
+            case 34: unpack<34>(in128, out128, outCount128); break;
+            case 35: unpack<35>(in128, out128, outCount128); break;
+            case 36: unpack<36>(in128, out128, outCount128); break;
+            case 37: unpack<37>(in128, out128, outCount128); break;
+            case 38: unpack<38>(in128, out128, outCount128); break;
+            case 39: unpack<39>(in128, out128, outCount128); break;
+            case 40: unpack<40>(in128, out128, outCount128); break;
+            case 41: unpack<41>(in128, out128, outCount128); break;
+            case 42: unpack<42>(in128, out128, outCount128); break;
+            case 43: unpack<43>(in128, out128, outCount128); break;
+            case 44: unpack<44>(in128, out128, outCount128); break;
+            case 45: unpack<45>(in128, out128, outCount128); break;
+            case 46: unpack<46>(in128, out128, outCount128); break;
+            case 47: unpack<47>(in128, out128, outCount128); break;
+            case 48: unpack<48>(in128, out128, outCount128); break;
+            case 49: unpack<49>(in128, out128, outCount128); break;
+            case 50: unpack<50>(in128, out128, outCount128); break;
+            case 51: unpack<51>(in128, out128, outCount128); break;
+            case 52: unpack<52>(in128, out128, outCount128); break;
+            case 53: unpack<53>(in128, out128, outCount128); break;
+            case 54: unpack<54>(in128, out128, outCount128); break;
+            case 55: unpack<55>(in128, out128, outCount128); break;
+            case 56: unpack<56>(in128, out128, outCount128); break;
+            case 57: unpack<57>(in128, out128, outCount128); break;
+            case 58: unpack<58>(in128, out128, outCount128); break;
+            case 59: unpack<59>(in128, out128, outCount128); break;
+            case 60: unpack<60>(in128, out128, outCount128); break;
+            case 61: unpack<61>(in128, out128, outCount128); break;
+            case 62: unpack<62>(in128, out128, outCount128); break;
+            case 63: unpack<63>(in128, out128, outCount128); break;
+            case 64: unpack<64>(in128, out128, outCount128); break;
+        }
+    }
+    
 }
 
 #endif //MORPHSTORE_CORE_MORPHING_VBP_ROUTINES_H
