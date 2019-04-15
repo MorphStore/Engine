19ebb15cd54b04f22b35a09b69b4d6872c3ad81f
Patrick Damme <patrick.damme@tu-dresden.de>
2019-04-15 18:14:03 +0200

    Refactoring: The project-operator is wrapped by a struct now.
    - Necessary, because when processing compressed data, the operator needs additional template parameters (hence, partial template specialization).
    - The struct is named project_t.
    - There is a convenience function named project delegating to the struct's static apply function.

--- include/core/operators/interfaces/project.h
+++ include/core/operators/interfaces/project.h
@@ -18,7 +18,6 @@
 /**
  * @file project.h
  * @brief The template-based interface of the project-operator.
- * @todo TODOS?
  */
 
 #ifndef MORPHSTORE_CORE_OPERATORS_INTERFACES_PROJECT_H
@@ -30,18 +29,54 @@
 namespace morphstore {
     
 /**
- * Project-operator. Extracts the data elements at the given positions from the
- * given data column.
+ * @brief A struct wrapping the actual project-operator.
  * 
- * Example:
- * - inDataCol:  [11, 44, 22, 33, 11]
- * - inPosCol:   [     1,      3,  4]
- * - outDataCol: [    44,     33, 11]
+ * This is necessary to enable partial template specialization, which is
+ * required, since some compressed formats have their own template parameters.
+ */
+template<
+        processing_style_t t_ps,
+        class t_out_data_f,
+        class t_in_data_f,
+        class t_in_pos_f
+>
+struct project_t {
+    /**
+     * @brief Project-operator. Extracts the data elements at the given
+     * positions from the given data column.
+     * 
+     * Example:
+     * - `inDataCol`:  `[11, 44, 22, 33, 11]`
+     * - `inPosCol`:   `[     1,      3,  4]`
+     * - `outDataCol`: `[    44,     33, 11]`
+     * 
+     * This function is deleted by default, to guarantee that using this struct
+     * with a format combination it is not specialized for causes a compiler
+     * error, not a linker error.
+     * 
+     * @param inDataCol The column to extract from.
+     * @param inPosCol The column containing the positions to extract.
+     * @return A column containing the extracted data elements, i.e., as many
+     * as in `inPosCol`.
+     */
+    static
+    const column<t_out_data_f> *
+    apply(
+            const column<t_in_data_f> * const inDataCol,
+            const column<t_in_pos_f> * const inPosCol
+    ) = delete;
+};
+
+/**
+ * @brief A convenience function wrapping the project-operator.
+ * 
+ * Extracts the data elements at the given positions from the given data
+ * column. See documentation of `project_t::apply` for details.
  * 
  * @param inDataCol The column to extract from.
  * @param inPosCol The column containing the positions to extract.
  * @return A column containing the extracted data elements, i.e., as many as in
- * inPosCol.
+ * `inPosCol`.
  */
 template<
         processing_style_t t_ps,
@@ -53,7 +88,12 @@ const column<t_out_data_f> *
 project(
         const column<t_in_data_f> * const inDataCol,
         const column<t_in_pos_f> * const inPosCol
-);
+) {
+    return project_t<t_ps, t_out_data_f, t_in_data_f, t_in_pos_f>::apply(
+            inDataCol,
+            inPosCol
+    );
+}
 
 }
 #endif //MORPHSTORE_CORE_OPERATORS_INTERFACES_PROJECT_H
