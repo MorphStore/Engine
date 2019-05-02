566101514da3ccee2450ba1588cac62f44dab7fd
Patrick Damme <patrick.damme@tu-dresden.de>
2019-05-02 16:55:36 +0200

    Some early compressed variants of the project-operator.
    - The input positions column can be compressed in the format static_vbp_f.
    - Purely scalar implementations.
    - Still a lot to be done w.r.t. efficiency, code complexity etc..

--- test/core/operators/project_benchmark_test.cpp
+++ test/core/operators/project_benchmark_test.cpp
@@ -24,13 +24,18 @@
 #include <core/memory/mm_glob.h>
 #include <core/morphing/format.h>
 #include <core/morphing/morph.h>
+#include <core/morphing/static_vbp.h>
+#include <core/operators/scalar/project_compr.h>
 #include <core/operators/scalar/project_uncompr.h>
 #include <core/operators/vectorized/project_uncompr.h>
 #include <core/storage/column.h>
 #include <core/storage/column_gen.h>
 #include <core/utils/basic_types.h>
 #include <core/utils/equality_check.h>
+#include <core/utils/helper.h>
+#include <core/utils/math.h>
 #include <core/utils/monitoring.h>
+#include <core/utils/preprocessor.h>
 #include <core/utils/printing.h>
 #include <core/utils/processing_style.h>
 
@@ -39,6 +44,7 @@
 #include <random>
 #include <tuple>
 #include <type_traits>
+#include <map>
 #include <vector>
 
 using namespace morphstore;
@@ -146,8 +152,18 @@ typedef const column<uncompr_f> *
             t_in_pos_f \
     >, \
     #t_ps_project, \
-    #t_in_pos_f \
+    STR_EVAL_MACROS(t_in_pos_f) \
 }
+
+typedef std::tuple<
+    project_fp_t, const std::string, const std::string
+> variant_t;
+    
+// This must be a macro, because if it is a C++ variable/constant, then the
+// output will always contain the identifier/expression, but not the value.
+// The value must be a literal here, it can be calculated as
+// sizeof(__m128i) / sizeof(uint64_t) .
+#define STEP_128 2
 
 /**
  * @brief Tests if all considered variants of the project-operator yield
@@ -196,13 +212,91 @@ bool evaluate_variants(size_t p_InDataCount, size_t p_InPosCount) {
     // Each variant is a pair. The first element is a pointer to the function
     // to call, the second element is the name of the variant (to be used as
     // the monitoring counter name).
-    std::vector<
-            std::tuple<project_fp_t, std::string, std::string>
-    > variants = {
+    std::vector<variant_t> variants = {
         MAKE_VARIANT(scalar, scalar, uncompr_f),
         MAKE_VARIANT(scalar, vec128, uncompr_f),
         MAKE_VARIANT(scalar, vec256, uncompr_f)
     };
+    // These variants can only be executed if their static bit width is high
+    // enough to represent the highest possible position.
+    std::map<unsigned, std::vector<variant_t> > staticVPBVariantsByBw = {
+        // Generated with Python:
+        // for bw in range(1, 64+1):
+        //   print("{{{: >2}, {{MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<{: >2}, STEP_128>))}}}}{}".format(bw, bw, "," if bw < 64 else ""))
+        { 1, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f< 1, STEP_128>))}},
+        { 2, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f< 2, STEP_128>))}},
+        { 3, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f< 3, STEP_128>))}},
+        { 4, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f< 4, STEP_128>))}},
+        { 5, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f< 5, STEP_128>))}},
+        { 6, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f< 6, STEP_128>))}},
+        { 7, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f< 7, STEP_128>))}},
+        { 8, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f< 8, STEP_128>))}},
+        { 9, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f< 9, STEP_128>))}},
+        {10, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<10, STEP_128>))}},
+        {11, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<11, STEP_128>))}},
+        {12, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<12, STEP_128>))}},
+        {13, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<13, STEP_128>))}},
+        {14, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<14, STEP_128>))}},
+        {15, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<15, STEP_128>))}},
+        {16, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<16, STEP_128>))}},
+        {17, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<17, STEP_128>))}},
+        {18, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<18, STEP_128>))}},
+        {19, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<19, STEP_128>))}},
+        {20, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<20, STEP_128>))}},
+        {21, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<21, STEP_128>))}},
+        {22, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<22, STEP_128>))}},
+        {23, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<23, STEP_128>))}},
+        {24, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<24, STEP_128>))}},
+        {25, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<25, STEP_128>))}},
+        {26, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<26, STEP_128>))}},
+        {27, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<27, STEP_128>))}},
+        {28, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<28, STEP_128>))}},
+        {29, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<29, STEP_128>))}},
+        {30, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<30, STEP_128>))}},
+        {31, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<31, STEP_128>))}},
+        {32, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<32, STEP_128>))}},
+        {33, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<33, STEP_128>))}},
+        {34, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<34, STEP_128>))}},
+        {35, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<35, STEP_128>))}},
+        {36, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<36, STEP_128>))}},
+        {37, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<37, STEP_128>))}},
+        {38, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<38, STEP_128>))}},
+        {39, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<39, STEP_128>))}},
+        {40, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<40, STEP_128>))}},
+        {41, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<41, STEP_128>))}},
+        {42, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<42, STEP_128>))}},
+        {43, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<43, STEP_128>))}},
+        {44, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<44, STEP_128>))}},
+        {45, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<45, STEP_128>))}},
+        {46, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<46, STEP_128>))}},
+        {47, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<47, STEP_128>))}},
+        {48, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<48, STEP_128>))}},
+        {49, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<49, STEP_128>))}},
+        {50, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<50, STEP_128>))}},
+        {51, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<51, STEP_128>))}},
+        {52, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<52, STEP_128>))}},
+        {53, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<53, STEP_128>))}},
+        {54, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<54, STEP_128>))}},
+        {55, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<55, STEP_128>))}},
+        {56, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<56, STEP_128>))}},
+        {57, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<57, STEP_128>))}},
+        {58, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<58, STEP_128>))}},
+        {59, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<59, STEP_128>))}},
+        {60, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<60, STEP_128>))}},
+        {61, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<61, STEP_128>))}},
+        {62, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<62, STEP_128>))}},
+        {63, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<63, STEP_128>))}},
+        {64, {MAKE_VARIANT(vec128, scalar, SINGLE_ARG(static_vbp_f<64, STEP_128>))}}
+    };
+
+    // Add all operator variants using the static_vbp format with a high enough
+    // bit width.
+    const unsigned minBw = effective_bitwidth(p_InDataCount - 1);
+    for(const auto & vs : staticVPBVariantsByBw)
+        if(vs.first >= minBw)
+            for(const auto & v : vs.second)
+                variants.push_back(v);
+    
     // Just for the sake of a nicely formatted output.
     size_t maxLen = 0;
     for(auto v : variants) {
@@ -288,6 +382,8 @@ bool evaluate_variants(size_t p_InDataCount, size_t p_InPosCount) {
     
     return allGood;
 }
+
+#undef STEP_128
 
 int main(void) {
 #ifdef MSV_NO_SELFMANAGED_MEMORY
