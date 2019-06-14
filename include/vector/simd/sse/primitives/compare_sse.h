2388681fc29ec97897f0aacb219ffb0681820121
Patrick Damme <patrick.damme@tu-dresden.de>
2019-06-14 12:44:28 +0200

    Bugfix: Removed extra comma.

--- include/vector/simd/sse/primitives/compare_sse.h
+++ include/vector/simd/sse/primitives/compare_sse.h
@@ -119,7 +119,7 @@ namespace vector{
       MSV_CXX_ATTRIBUTE_FORCE_INLINE
       static uint8_t
       apply(
-         typename sse<v128<uint64_t>>::mask_t const & p_mask,
+         typename sse<v128<uint64_t>>::mask_t const & p_mask
       ) {
          trace( "[VECTOR] - Count matches in a comparison mask (sse)" );
          // @todo Which one is faster?
