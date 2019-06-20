2bd552de8b3d5d9986ba44378f7484d217870422
Lukas Landgraf <llandgraf317@gmail.com>
2019-06-20 12:16:23 +0200

    Expanded testing for custom mm

--- test/core/memory/mmap_test.cpp
+++ test/core/memory/mmap_test.cpp
@@ -1,16 +1,22 @@
+//#define USE_MMAP_MM
+
 #include <core/memory/management/abstract_mm.h>
 #include <core/memory/management/mmap_mm.h>
 #include <core/memory/management/paged_mm.h>
 
-#include <core/memory/global/mm_hooks.h>
+//#include <core/memory/global/mm_hooks.h>
+#include <core/memory/morphstore_mm.h>
 /*#include <core/memory/management/allocators/global_scope_allocator.h>
 #include <core/memory/management/query_mm.h>*/
-#include <core/memory/global/mm_override.h>
+//#include <core/memory/global/mm_override.h>
+    // load capacity output
 
 #include <cstdio>
 #include <vector>
 #include <chrono>
 #include <iostream>
+#include <map>
+
 
 namespace morphstore {
 
@@ -36,8 +42,39 @@ int main(int /*argc*/, char** /*argv*/) {
 
     morphstore::paged_memory_manager& page_instance = morphstore::paged_memory_manager::getGlobalInstance();
 
-    /*void* obj_ptr = page_instance.allocate(32);
-    page_instance.deallocate(obj_ptr);
+    void* obj_ptr = mm_malloc(32);
+    *reinterpret_cast<uint64_t*>(obj_ptr) = 64;
+    obj_ptr = mm_realloc(obj_ptr, 64);
+
+    reinterpret_cast<uint64_t*>(obj_ptr)[5] = 64;
+    std::cout << "Current val: " << *reinterpret_cast<uint64_t*>(obj_ptr) << std::endl;
+    assert(*reinterpret_cast<uint64_t*>(obj_ptr) == 64);
+
+    obj_ptr = mm_realloc(obj_ptr, 8);
+    assert(*reinterpret_cast<uint64_t*>(obj_ptr) == 64);
+    mm_free(obj_ptr);
+    std::cout << "Done realloc" << std::endl;
+
+    unsigned inDataCount = 234000;
+    uint64_t* columns[128];
+    for (int j = 0; j < 128; j+=2) {
+        columns[j] = reinterpret_cast<uint64_t*>( mm_malloc(inDataCount * sizeof(uint64_t)) );
+        columns[j+1] = reinterpret_cast<uint64_t*>( mm_malloc(inDataCount * sizeof(uint64_t)) );
+        
+        std::map<std::pair<uint64_t, uint64_t>, uint64_t> groupIds;
+        for(unsigned i = 0; i < inDataCount; i++) {
+            std::pair<uint64_t,uint64_t> pair = std::make_pair(columns[j][i], columns[j+1][i]);
+            uint64_t & groupId = groupIds[pair];
+            if(!groupId) { // The data item(key) was not found.
+                groupId = groupIds.size();
+            }
+        }
+    }
+
+    for (int j = 0; j < 128; j++) {
+        delete[] columns[j];
+    }
+
 
     //obj_ptr = malloc(72704);
     //free(obj_ptr);*/
@@ -63,42 +100,57 @@ int main(int /*argc*/, char** /*argv*/) {
         ptr = reinterpret_cast<void*>(instance.allocateContinuous());
         page_instance.setCurrentChunk(ptr);
 
+        std::chrono::time_point<std::chrono::system_clock> alloc_start, alloc_end, dealloc_start, dealloc_end;
+
         auto start = std::chrono::system_clock::now();
         for (int k = 0; k < 3; ++k) {
 
+            alloc_start = std::chrono::system_clock::now();
             for (unsigned int i = 0; i < ARRAY_LENGTH; ++i) {
-                ptrs[i] = mm_malloc(object_sizes[(j + i) % 7]);
+                ptrs[i] = mm_malloc(object_sizes[j]);
                 if (ptrs[i] == nullptr)
                     std::cerr << "Pointer " << i << " got returned as zero" << std::endl;
                 *reinterpret_cast<uint64_t*>(ptrs[i]) = 4;
             }
+            alloc_end = std::chrono::system_clock::now();
 
-            for (int i = 0; i < ARRAY_LENGTH; ++i) {
+            dealloc_start = std::chrono::system_clock::now();
+            for (unsigned int i = 0; i < ARRAY_LENGTH; ++i) {
                 mm_free(reinterpret_cast<char*>(ptrs[i]));
             }
+            dealloc_end = std::chrono::system_clock::now();
+
         }
         auto end = std::chrono::system_clock::now();
         std::chrono::duration<double> elapsed_seconds = end - start;
 
+        std::chrono::duration<double> elapsed_sec_alloc = alloc_end - alloc_start;
+        std::chrono::duration<double> elapsed_sec_dealloc = dealloc_end - dealloc_start;
+        std::cout << "Alloc: " << elapsed_sec_alloc.count() << ", dealloc: " << elapsed_sec_dealloc.count() << std::endl;
         std::cout << "Needed " << elapsed_seconds.count() << " for " << ARRAY_LENGTH << " allocations and deallocations using the custom mm" << std::endl;
 
         start = std::chrono::system_clock::now();
         for (int k = 0; k < 3; ++k) {
+
+            alloc_start = std::chrono::system_clock::now();
             for (unsigned int i = 0; i < ARRAY_LENGTH; ++i) {
-                ptrs[i] = malloc(object_sizes[(i + j) % 7]);
-                //std::cout << "Iteration " << std::dec << i << std::endl;
+                ptrs[i] = morphstore::stdlib_malloc(object_sizes[j]);
                 if (ptrs[i] == nullptr)
                     std::cerr << "Pointer " << i << " got returned as zero" << std::endl;
                 *reinterpret_cast<uint64_t*>(ptrs[i]) = 4;
             }
-            //std::cout << "Beginning deallocation" << std::endl;
+            alloc_end = std::chrono::system_clock::now();
+            dealloc_start = std::chrono::system_clock::now();
             for (unsigned int i = 0; i < ARRAY_LENGTH; ++i) {
-                //std::cout << "it: " << i << std::endl;
-                free(reinterpret_cast<char*>(ptrs[i]));
+                morphstore::stdlib_free(reinterpret_cast<char*>(ptrs[i]));
             }
+            dealloc_end = std::chrono::system_clock::now();
         }
         end = std::chrono::system_clock::now();
         elapsed_seconds = end - start;
+        elapsed_sec_alloc = alloc_end - alloc_start;
+        elapsed_sec_dealloc = dealloc_end - dealloc_start;
+        std::cout << "Alloc: " << elapsed_sec_alloc.count() << ", dealloc: " << elapsed_sec_dealloc.count() << std::endl;
         std::cout << "Needed " << elapsed_seconds.count() << " for " << ARRAY_LENGTH << " allocations and deallocations using std allocator" << std::endl;
     }
     assert(false);
