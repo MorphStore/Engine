ba5a2db6fc346eb79ba139f2e3c29b3d9432e9ae
Patrick Damme <patrick.damme@tu-dresden.de>
2019-08-15 19:53:32 +0200

    More useful test data for random_access_test.cpp.

--- test/core/operators/random_access_test.cpp
+++ test/core/operators/random_access_test.cpp
@@ -18,6 +18,8 @@
 /**
  * @file random_access_test.cpp
  * @brief Tests of the implementations of the random_read_access-interface.
+ * @todo Probably we should merge this with some test or benchmark of the
+ * project-operator.
  */
 
 #include <core/memory/noselfmanaging_helper.h>
@@ -51,25 +53,6 @@ using namespace vectorlib;
 // ****************************************************************************
 // Some simple operatos for testing the random access interface.
 // ****************************************************************************
-
-// As the reference.
-const column<uncompr_f> * copy(
-        const column<uncompr_f> * p_InDataCol,
-        const column<uncompr_f> * p_InPosCol
-) {
-    const uint8_t * inData = p_InDataCol->get_data();
-    
-    const size_t inPosCount = p_InPosCol->get_count_values();
-    
-    const size_t outSize = get_size_max_byte_any_len<uncompr_f>(inPosCount);
-    auto outDataCol = new column<uncompr_f>(outSize);
-    uint8_t * outData = outDataCol->get_data();
-    
-    memcpy(outData, inData, outSize);
-    
-    outDataCol->set_meta_data(inPosCount, outSize);
-    return outDataCol;
-}
 
 // Simplified project-operator.
 template<class t_ve, class t_in_data_f>
@@ -122,16 +105,6 @@ const column<uncompr_f> * simple_project(
 template<class t_varex_t, unsigned t_Bw>
 std::vector<typename t_varex_t::variant_t> make_variants() {
     return {
-        // Reference variant.
-        {
-            new typename t_varex_t::operator_wrapper::template for_output_formats<uncompr_f>::template for_input_formats<uncompr_f, uncompr_f>(
-                &copy
-            ),
-            "copy",
-            "copy"
-        },
-        
-        // Compressed variants.
         MAKE_VARIANT(scalar<v64<uint64_t>>, uncompr_f),
         MAKE_VARIANT(scalar<v64<uint64_t>>, SINGLE_ARG(static_vbp_f<vbp_l<t_Bw, 1>>)),
 #ifdef SSE
@@ -161,103 +134,123 @@ int main(void) {
     
     using varex_t = variant_executor_helper<1, 2>::type
         ::for_variant_params<std::string, std::string>
-        ::for_setting_params<unsigned, size_t>;
+        ::for_setting_params<unsigned, size_t, size_t>;
     varex_t varex(
             {},
             {"vector_extension", "format"},
-            {"bit width", "countDataLog"}
+            {"bit width", "countDataLog", "countPosLog"}
     );
     
-    for(size_t countDataLog : {512, 512 * 1024}) {
-        for(
-                unsigned bw = effective_bitwidth(countDataLog - 1);
-                bw <= std::numeric_limits<uint64_t>::digits;
-                bw++
-        ) {
-            std::vector<varex_t::variant_t> variants;
-            switch(bw) {
-                // Generated with python:
-                // for bw in range(1, 64+1):
-                //   print("case {: >2}: variants = make_variants<varex_t, {: >2}>(); break;".format(bw, bw))
-                case  1: variants = make_variants<varex_t,  1>(); break;
-                case  2: variants = make_variants<varex_t,  2>(); break;
-                case  3: variants = make_variants<varex_t,  3>(); break;
-                case  4: variants = make_variants<varex_t,  4>(); break;
-                case  5: variants = make_variants<varex_t,  5>(); break;
-                case  6: variants = make_variants<varex_t,  6>(); break;
-                case  7: variants = make_variants<varex_t,  7>(); break;
-                case  8: variants = make_variants<varex_t,  8>(); break;
-                case  9: variants = make_variants<varex_t,  9>(); break;
-                case 10: variants = make_variants<varex_t, 10>(); break;
-                case 11: variants = make_variants<varex_t, 11>(); break;
-                case 12: variants = make_variants<varex_t, 12>(); break;
-                case 13: variants = make_variants<varex_t, 13>(); break;
-                case 14: variants = make_variants<varex_t, 14>(); break;
-                case 15: variants = make_variants<varex_t, 15>(); break;
-                case 16: variants = make_variants<varex_t, 16>(); break;
-                case 17: variants = make_variants<varex_t, 17>(); break;
-                case 18: variants = make_variants<varex_t, 18>(); break;
-                case 19: variants = make_variants<varex_t, 19>(); break;
-                case 20: variants = make_variants<varex_t, 20>(); break;
-                case 21: variants = make_variants<varex_t, 21>(); break;
-                case 22: variants = make_variants<varex_t, 22>(); break;
-                case 23: variants = make_variants<varex_t, 23>(); break;
-                case 24: variants = make_variants<varex_t, 24>(); break;
-                case 25: variants = make_variants<varex_t, 25>(); break;
-                case 26: variants = make_variants<varex_t, 26>(); break;
-                case 27: variants = make_variants<varex_t, 27>(); break;
-                case 28: variants = make_variants<varex_t, 28>(); break;
-                case 29: variants = make_variants<varex_t, 29>(); break;
-                case 30: variants = make_variants<varex_t, 30>(); break;
-                case 31: variants = make_variants<varex_t, 31>(); break;
-                case 32: variants = make_variants<varex_t, 32>(); break;
-                case 33: variants = make_variants<varex_t, 33>(); break;
-                case 34: variants = make_variants<varex_t, 34>(); break;
-                case 35: variants = make_variants<varex_t, 35>(); break;
-                case 36: variants = make_variants<varex_t, 36>(); break;
-                case 37: variants = make_variants<varex_t, 37>(); break;
-                case 38: variants = make_variants<varex_t, 38>(); break;
-                case 39: variants = make_variants<varex_t, 39>(); break;
-                case 40: variants = make_variants<varex_t, 40>(); break;
-                case 41: variants = make_variants<varex_t, 41>(); break;
-                case 42: variants = make_variants<varex_t, 42>(); break;
-                case 43: variants = make_variants<varex_t, 43>(); break;
-                case 44: variants = make_variants<varex_t, 44>(); break;
-                case 45: variants = make_variants<varex_t, 45>(); break;
-                case 46: variants = make_variants<varex_t, 46>(); break;
-                case 47: variants = make_variants<varex_t, 47>(); break;
-                case 48: variants = make_variants<varex_t, 48>(); break;
-                case 49: variants = make_variants<varex_t, 49>(); break;
-                case 50: variants = make_variants<varex_t, 50>(); break;
-                case 51: variants = make_variants<varex_t, 51>(); break;
-                case 52: variants = make_variants<varex_t, 52>(); break;
-                case 53: variants = make_variants<varex_t, 53>(); break;
-                case 54: variants = make_variants<varex_t, 54>(); break;
-                case 55: variants = make_variants<varex_t, 55>(); break;
-                case 56: variants = make_variants<varex_t, 56>(); break;
-                case 57: variants = make_variants<varex_t, 57>(); break;
-                case 58: variants = make_variants<varex_t, 58>(); break;
-                case 59: variants = make_variants<varex_t, 59>(); break;
-                case 60: variants = make_variants<varex_t, 60>(); break;
-                case 61: variants = make_variants<varex_t, 61>(); break;
-                case 62: variants = make_variants<varex_t, 62>(); break;
-                case 63: variants = make_variants<varex_t, 63>(); break;
-                case 64: variants = make_variants<varex_t, 64>(); break;
-            }
-
-            varex.print_datagen_started();
-            auto origDataCol = generate_sorted_unique(countDataLog);
-            auto origPosCol = generate_sorted_unique(countDataLog);
-            varex.print_datagen_done();
-
-            varex.execute_variants(
-                    variants, bw, countDataLog, origDataCol, origPosCol
-            );
-
-            delete origDataCol;
-            delete origPosCol;
+    size_t countDataLog = 32 * 1024;
+    size_t countPosLog = 16 * 1024;
+    
+    varex.print_datagen_started();
+    auto origPosCol = generate_with_distr(
+            countPosLog,
+            std::uniform_int_distribution<uint64_t>(0, countDataLog - 1),
+            false
+    );
+    varex.print_datagen_done();
+    
+    for(
+            unsigned bw = 1;
+            bw <= std::numeric_limits<uint64_t>::digits;
+            bw++
+    ) {
+        std::vector<varex_t::variant_t> variants;
+        switch(bw) {
+            // Generated with python:
+            // for bw in range(1, 64+1):
+            //   print("case {: >2}: variants = make_variants<varex_t, {: >2}>(); break;".format(bw, bw))
+            case  1: variants = make_variants<varex_t,  1>(); break;
+            case  2: variants = make_variants<varex_t,  2>(); break;
+            case  3: variants = make_variants<varex_t,  3>(); break;
+            case  4: variants = make_variants<varex_t,  4>(); break;
+            case  5: variants = make_variants<varex_t,  5>(); break;
+            case  6: variants = make_variants<varex_t,  6>(); break;
+            case  7: variants = make_variants<varex_t,  7>(); break;
+            case  8: variants = make_variants<varex_t,  8>(); break;
+            case  9: variants = make_variants<varex_t,  9>(); break;
+            case 10: variants = make_variants<varex_t, 10>(); break;
+            case 11: variants = make_variants<varex_t, 11>(); break;
+            case 12: variants = make_variants<varex_t, 12>(); break;
+            case 13: variants = make_variants<varex_t, 13>(); break;
+            case 14: variants = make_variants<varex_t, 14>(); break;
+            case 15: variants = make_variants<varex_t, 15>(); break;
+            case 16: variants = make_variants<varex_t, 16>(); break;
+            case 17: variants = make_variants<varex_t, 17>(); break;
+            case 18: variants = make_variants<varex_t, 18>(); break;
+            case 19: variants = make_variants<varex_t, 19>(); break;
+            case 20: variants = make_variants<varex_t, 20>(); break;
+            case 21: variants = make_variants<varex_t, 21>(); break;
+            case 22: variants = make_variants<varex_t, 22>(); break;
+            case 23: variants = make_variants<varex_t, 23>(); break;
+            case 24: variants = make_variants<varex_t, 24>(); break;
+            case 25: variants = make_variants<varex_t, 25>(); break;
+            case 26: variants = make_variants<varex_t, 26>(); break;
+            case 27: variants = make_variants<varex_t, 27>(); break;
+            case 28: variants = make_variants<varex_t, 28>(); break;
+            case 29: variants = make_variants<varex_t, 29>(); break;
+            case 30: variants = make_variants<varex_t, 30>(); break;
+            case 31: variants = make_variants<varex_t, 31>(); break;
+            case 32: variants = make_variants<varex_t, 32>(); break;
+            case 33: variants = make_variants<varex_t, 33>(); break;
+            case 34: variants = make_variants<varex_t, 34>(); break;
+            case 35: variants = make_variants<varex_t, 35>(); break;
+            case 36: variants = make_variants<varex_t, 36>(); break;
+            case 37: variants = make_variants<varex_t, 37>(); break;
+            case 38: variants = make_variants<varex_t, 38>(); break;
+            case 39: variants = make_variants<varex_t, 39>(); break;
+            case 40: variants = make_variants<varex_t, 40>(); break;
+            case 41: variants = make_variants<varex_t, 41>(); break;
+            case 42: variants = make_variants<varex_t, 42>(); break;
+            case 43: variants = make_variants<varex_t, 43>(); break;
+            case 44: variants = make_variants<varex_t, 44>(); break;
+            case 45: variants = make_variants<varex_t, 45>(); break;
+            case 46: variants = make_variants<varex_t, 46>(); break;
+            case 47: variants = make_variants<varex_t, 47>(); break;
+            case 48: variants = make_variants<varex_t, 48>(); break;
+            case 49: variants = make_variants<varex_t, 49>(); break;
+            case 50: variants = make_variants<varex_t, 50>(); break;
+            case 51: variants = make_variants<varex_t, 51>(); break;
+            case 52: variants = make_variants<varex_t, 52>(); break;
+            case 53: variants = make_variants<varex_t, 53>(); break;
+            case 54: variants = make_variants<varex_t, 54>(); break;
+            case 55: variants = make_variants<varex_t, 55>(); break;
+            case 56: variants = make_variants<varex_t, 56>(); break;
+            case 57: variants = make_variants<varex_t, 57>(); break;
+            case 58: variants = make_variants<varex_t, 58>(); break;
+            case 59: variants = make_variants<varex_t, 59>(); break;
+            case 60: variants = make_variants<varex_t, 60>(); break;
+            case 61: variants = make_variants<varex_t, 61>(); break;
+            case 62: variants = make_variants<varex_t, 62>(); break;
+            case 63: variants = make_variants<varex_t, 63>(); break;
+            case 64: variants = make_variants<varex_t, 64>(); break;
         }
+
+        varex.print_datagen_started();
+        auto origDataCol = generate_with_distr(
+                countDataLog,
+                std::uniform_int_distribution<uint64_t>(
+                        0, bitwidth_max<uint64_t>(bw)
+                ),
+                false
+        );
+        varex.print_datagen_done();
+
+        varex.execute_variants(
+                variants,
+                bw,
+                countDataLog,
+                countPosLog,
+                origDataCol,
+                origPosCol
+        );
+
+        delete origDataCol;
     }
+    
+    delete origPosCol;
     
     varex.done();
     
