76e41bc251109f2b803d4a7c8a627b845e9d54e8
Tim Pfeiffer <timtim.pfeiffer@gmail.com>
2019-08-27 11:12:42 +0200

    added smart pointer for ldbc-import object

--- test/core/storage/graph/adjacencylist/ldbc_graph_adjacencylist.cpp
+++ test/core/storage/graph/adjacencylist/ldbc_graph_adjacencylist.cpp
@@ -37,14 +37,14 @@ int main( void ){
     auto start = std::chrono::high_resolution_clock::now(); // For measuring the execution time
 
     // when using workstation @ TUD: social network directory: "/home/s8069724/s8069724-home/Dokumente/social_network/"
-    //morphstore::LDBCImport ldbcImport("/opt/ldbc_snb_datagen-0.2.8/social_network/");
-    morphstore::LDBCImport ldbcImport("/home/s8069724/s8069724-home/Dokumente/social_network/");
+    std::unique_ptr<morphstore::LDBCImport> ldbcImport = std::make_unique<morphstore::LDBCImport>(("/opt/ldbc_snb_datagen-0.2.8/social_network/"));
+    //morphstore::LDBCImport ldbcImport("/home/s8069724/s8069724-home/Dokumente/social_network/");
 
     // Graph init:
     std::unique_ptr<morphstore::Graph> g1 = std::make_unique<morphstore::AdjacencyList>();
 
     // generate vertices & edges from LDBC files and insert into graph
-    ldbcImport.import(g1);
+    ldbcImport->import(g1);
 
     // measuring time:
     auto finish = std::chrono::high_resolution_clock::now(); // For measuring the execution time
@@ -60,7 +60,6 @@ int main( void ){
     g1->print_vertex_by_id(100168);
     g1->print_vertex_by_id(2000100);
      */
-    g1->print_neighbors_of_vertex(100449);
 
     // calculate size of social graph
     //std::cout << "Size of social network: " << socialGraph.get_size_of_graph() << " Bytes\n";
