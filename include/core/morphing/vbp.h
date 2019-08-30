e71bf9407fd9803affe4e1dc0117727f656069cb
Patrick Damme <patrick.damme@tu-dresden.de>
2019-08-30 18:17:15 +0200

    Added some variants for random read access on horizontally bit-packed data (vbp_l with a step of 1), tailored to certain classes of bit widths.

--- include/core/morphing/vbp.h
+++ include/core/morphing/vbp.h
@@ -37,6 +37,7 @@
 #include <vector/vector_primitives.h>
 
 #include <limits>
+#include <type_traits>
 
 #include <cstdint>
 
@@ -403,7 +404,258 @@ namespace morphstore {
     // ------------------------------------------------------------------------
     
     namespace _random_read_access_variants {
+        // @todo Prevent unsupported bit widths with static_asserts.
+        // @todo Think about the restrictions caused by the precision of the
+        // multiplication.
 
+        /**
+         * @brief `random_read_access` for `vbp_l` with a step of 1 (horizontal
+         * bit packing). Works only if the bit width is a power of two and
+         * greater than or equal to 8, i.e., for 8, 16, 32, 64 bit.
+         */
+        template<class t_vector_extension, unsigned t_Bw>
+        class rra_vbp_l_hor_8_16_32_64 {
+            using t_ve = t_vector_extension;
+            IMPORT_VECTOR_BOILER_PLATE(t_ve)
+            
+            const base_t * const m_Data;
+            
+            static const vector_t m_MaskDecompr;
+            
+        public:
+            rra_vbp_l_hor_8_16_32_64(const base_t * p_Data) : m_Data(p_Data) {
+                //
+            }
+            
+            MSV_CXX_ATTRIBUTE_FORCE_INLINE
+            vector_t get(const vector_t & p_Positions) {
+                using namespace vectorlib;
+                
+                // For 64-bit we do not even need the mask, but for 64-bit, we
+                // should rather use uncompressed data anyway.
+                return bitwise_and<t_ve>(
+                        gather<
+                                t_ve,
+                                vector_base_t_granularity::value,
+                                t_Bw / bitsPerByte
+                        >(m_Data, p_Positions),
+                        m_MaskDecompr
+                );
+            }
+        };
+
+        template<class t_ve, unsigned t_Bw>
+        const typename t_ve::vector_t rra_vbp_l_hor_8_16_32_64<
+                t_ve, t_Bw
+        >::m_MaskDecompr = vectorlib::set1<
+                t_ve, t_ve::vector_helper_t::granularity::value
+        >(bitwidth_max<uint64_t>(t_Bw));
+        
+        /**
+         * @brief `random_read_access` for `vbp_l` with a step of 1 (horizontal
+         * bit packing). Works only if the bit width is a power of two and less
+         * than or equal to 8, i.e., for 1, 2, 4, 8 bit.
+         */
+        template<class t_vector_extension, unsigned t_Bw>
+        class rra_vbp_l_hor_1_2_4_8 {
+            using t_ve = t_vector_extension;
+            IMPORT_VECTOR_BOILER_PLATE(t_ve)
+            
+            const base_t * const m_Data;
+            
+            static const unsigned m_Shift4DivCodesPerByte = shift_for_muldiv(
+                    bitsPerByte / t_Bw
+            );
+            static const unsigned m_Shift4MulBw = shift_for_muldiv(t_Bw);
+            
+            static const vector_t m_Mask4ModBitsPerByte;
+            static const vector_t m_MaskDecompr;
+            
+        public:
+            rra_vbp_l_hor_1_2_4_8(const base_t * p_Data) : m_Data(p_Data) {
+                //
+            }
+            
+            MSV_CXX_ATTRIBUTE_FORCE_INLINE
+            vector_t get(const vector_t & p_Positions) {
+                using namespace vectorlib;
+                
+                return bitwise_and<t_ve>(
+                        shift_right_individual<t_ve>::apply(
+                                gather<
+                                        t_ve,
+                                        vector_base_t_granularity::value,
+                                        1
+                                >(
+                                        m_Data,
+                                        shift_right<t_ve>::apply(
+                                                p_Positions,
+                                                m_Shift4DivCodesPerByte
+                                        )
+                                ),
+                                bitwise_and<t_ve>(
+                                        shift_left<t_ve>::apply(
+                                                p_Positions, m_Shift4MulBw
+                                        ),
+                                        m_Mask4ModBitsPerByte
+                                )
+                        ),
+                        m_MaskDecompr
+                );
+            }
+        };
+
+        template<class t_ve, unsigned t_Bw>
+        const typename t_ve::vector_t rra_vbp_l_hor_1_2_4_8<
+                t_ve, t_Bw
+        >::m_Mask4ModBitsPerByte = vectorlib::set1<
+                t_ve, t_ve::vector_helper_t::granularity::value
+        >(mask_for_mod(bitsPerByte));
+
+        template<class t_ve, unsigned t_Bw>
+        const typename t_ve::vector_t rra_vbp_l_hor_1_2_4_8<
+                t_ve, t_Bw
+        >::m_MaskDecompr = vectorlib::set1<
+                t_ve, t_ve::vector_helper_t::granularity::value
+        >(bitwidth_max<uint64_t>(t_Bw));
+        
+        /**
+         * @brief `random_read_access` for `vbp_l` with a step of 1 (horizontal
+         * bit packing). Works only if the bit width is a multiple of 8, i.e.,
+         * for 8, 16, 24, 32, 40, 48, 56, 64 bit.
+         */
+        template<class t_vector_extension, unsigned t_Bw>
+        class rra_vbp_l_hor_full_byte {
+            using t_ve = t_vector_extension;
+            IMPORT_VECTOR_BOILER_PLATE(t_ve)
+            
+            const base_t * const m_Data;
+            
+            static const vector_t m_ByteWidth;
+            static const vector_t m_MaskDecompr;
+            
+        public:
+            rra_vbp_l_hor_full_byte(const base_t * p_Data) : m_Data(p_Data) {
+                //
+            }
+            
+            MSV_CXX_ATTRIBUTE_FORCE_INLINE
+            vector_t get(const vector_t & p_Positions) {
+                using namespace vectorlib;
+                
+                // For 64-bit we do not even need the mask, but for 64-bit, we
+                // should rather use uncompressed data anyway.
+                return bitwise_and<t_ve>(
+                        gather<
+                                t_ve,
+                                vector_base_t_granularity::value,
+                                1
+                        >(
+                                m_Data,
+                                mul<t_ve>::apply(p_Positions, m_ByteWidth)
+                        ),
+                        m_MaskDecompr
+                );
+            }
+        };
+
+        template<class t_ve, unsigned t_Bw>
+        const typename t_ve::vector_t rra_vbp_l_hor_full_byte<
+                t_ve, t_Bw
+        >::m_ByteWidth = vectorlib::set1<
+                t_ve, t_ve::vector_helper_t::granularity::value
+        >(t_Bw / bitsPerByte);
+
+        template<class t_ve, unsigned t_Bw>
+        const typename t_ve::vector_t rra_vbp_l_hor_full_byte<
+                t_ve, t_Bw
+        >::m_MaskDecompr = vectorlib::set1<
+                t_ve, t_ve::vector_helper_t::granularity::value
+        >(bitwidth_max<uint64_t>(t_Bw));
+        
+        /**
+         * @brief `random_read_access` for `vbl_l` with a step of 1 (horizontal
+         * bit packing). Works for all bit widths except for 59, 61, 62, 63
+         * bit.
+         */
+        template<class t_vector_extension, unsigned t_Bw>
+        class rra_vbp_l_hor_all_but_59_61_62_63 {
+            using t_ve = t_vector_extension;
+            IMPORT_VECTOR_BOILER_PLATE(t_ve)
+            
+            const base_t * const m_Data;
+            
+            static const unsigned m_Shift4DivBitsPerByte = shift_for_muldiv(
+                    bitsPerByte
+            );
+            
+            static const vector_t m_Bw;
+            static const vector_t m_Mask4ModBitsPerByte;
+            static const vector_t m_MaskDecompr;
+            
+        public:
+            rra_vbp_l_hor_all_but_59_61_62_63(const base_t * p_Data)
+            : m_Data(p_Data) {
+                //
+            }
+            
+            MSV_CXX_ATTRIBUTE_FORCE_INLINE
+            vector_t get(const vector_t & p_Positions) {
+                using namespace vectorlib;
+                
+                const vector_t bitPositions = mul<t_ve>::apply(
+                        p_Positions, m_Bw
+                );
+                // For 64-bit we do not even need the mask, but for 64-bit, we
+                // should rather use uncompressed data anyway.
+                return bitwise_and<t_ve>(
+                        shift_right_individual<t_ve>::apply(
+                                gather<
+                                        t_ve,
+                                        vector_base_t_granularity::value,
+                                        1
+                                >(
+                                        m_Data,
+                                        shift_right<t_ve>::apply(
+                                                bitPositions,
+                                                m_Shift4DivBitsPerByte
+                                        )
+                                ),
+                                bitwise_and<t_ve>(
+                                        bitPositions,
+                                        m_Mask4ModBitsPerByte
+                                )
+                        ),
+                        m_MaskDecompr
+                );
+            }
+        };
+
+        template<class t_ve, unsigned t_Bw>
+        const typename t_ve::vector_t rra_vbp_l_hor_all_but_59_61_62_63<
+                t_ve, t_Bw
+        >::m_Bw = vectorlib::set1<
+                t_ve, t_ve::vector_helper_t::granularity::value
+        >(t_Bw);
+
+        template<class t_ve, unsigned t_Bw>
+        const typename t_ve::vector_t rra_vbp_l_hor_all_but_59_61_62_63<
+                t_ve, t_Bw
+        >::m_Mask4ModBitsPerByte = vectorlib::set1<
+                t_ve, t_ve::vector_helper_t::granularity::value
+        >(mask_for_mod(bitsPerByte));
+
+        template<class t_ve, unsigned t_Bw>
+        const typename t_ve::vector_t rra_vbp_l_hor_all_but_59_61_62_63<
+                t_ve, t_Bw
+        >::m_MaskDecompr = vectorlib::set1<
+                t_ve, t_ve::vector_helper_t::granularity::value
+        >(bitwidth_max<uint64_t>(t_Bw));
+
+        /**
+         * `random_read_access` for `vbp_l`. Works for all combinations of the
+         * step and the bit width.
+         */
         template<class t_vector_extension, unsigned t_Bw, unsigned t_Step>
         class rra_vbp_l_general {
             using t_ve = t_vector_extension;
@@ -526,10 +778,52 @@ namespace morphstore {
     
     template<class t_vector_extension, unsigned t_Bw, unsigned t_Step>
     struct random_read_access<t_vector_extension, vbp_l<t_Bw, t_Step> > {
-        // Alias to the only implementation we currently have.
-        using type = _random_read_access_variants::rra_vbp_l_general<
-                t_vector_extension, t_Bw, t_Step
-        >;
+        // Alias to the most efficient implementation we have, depending on the
+        // step and the bit width.
+        using type = typename std::conditional<
+                t_Step == 1,
+                // Horizontal layout.
+                typename std::conditional<
+                        is_power_of_two(t_Bw),
+                        // Bit width is a power of two.
+                        typename std::conditional<
+                                t_Bw < 8,
+                                // Bit width is a sub-byte power of two.
+                                _random_read_access_variants::rra_vbp_l_hor_1_2_4_8<
+                                        t_vector_extension, t_Bw
+                                >,
+                                // Bit width is a byte power of two.
+                                _random_read_access_variants::rra_vbp_l_hor_8_16_32_64<
+                                        t_vector_extension, t_Bw
+                                >
+                        >::type,
+                        // Bit width is not a power of two.
+                        typename std::conditional<
+                                t_Bw % 8 == 0,
+                                // Bit width is a full byte width.
+                                _random_read_access_variants::rra_vbp_l_hor_full_byte<
+                                        t_vector_extension, t_Bw
+                                >,
+                                // Bit width is not a full byte width.
+                                typename std::conditional<
+                                        !(t_Bw == 59 || (t_Bw >= 61 && t_Bw <= 63)),
+                                        // Bit width is still ok-ish.
+                                        _random_read_access_variants::rra_vbp_l_hor_all_but_59_61_62_63<
+                                                t_vector_extension, t_Bw
+                                        >,
+                                        // Bit width is really ugly.
+                                        _random_read_access_variants::rra_vbp_l_general<
+                                                t_vector_extension, t_Bw, t_Step
+                                        >
+                                >::type
+                        >::type
+                >::type,
+                // Vertical layout.
+                // @todo Implement some optimized variants here.
+                _random_read_access_variants::rra_vbp_l_general<
+                        t_vector_extension, t_Bw, t_Step
+                >
+        >::type;
     };
 }
 
