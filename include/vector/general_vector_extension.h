057b06a28f9651d2896e1f09776b0f49caa7fa12
DerSchmidt <Lennart.Schmidt.1995@gmail.com>
2020-01-15 14:24:41 +0100

    Temporary Scalar Bug fix

--- include/vector/general_vector_extension.h
+++ include/vector/general_vector_extension.h
@@ -26,12 +26,28 @@ namespace vectorlib {
       using granularity     = std::integral_constant<size_t, sizeof(T) << 3>;
    };
 
+   template<uint16_t BitWidth, typename T>
+   struct vector_view_2 {
+      vector_view_2() = delete;
+
+      using base_t          = T;
+      using size_bit        = std::integral_constant<uint16_t, BitWidth>;
+      using size_byte       = std::integral_constant<uint16_t, (BitWidth >> 3)>;
+      using alignment       = std::integral_constant<size_t, size_byte::value>;
+      using element_count   = std::integral_constant<size_t, 1>;
+      using granularity     = std::integral_constant<size_t, sizeof(T) << 3>;
+   };
+
    template<typename T>
    //using v1 = vector_view<(sizeof(T)<<3), T>;
-   using v64 = vector_view<64, T>;
+   using v64 = vector_view_2<64, T>;
    template<typename T>
-   using v32 = vector_view<32, T>;
+   using v32 = vector_view_2<32, T>;
    template<typename T>
+   // using v16 = vector_view<16, T>;
+   // template<typename T>
+   // using v8 = vector_view<8, T>;
+   // template<typename T>
    using v128 = vector_view<128, T>;
    template<typename T>
    using v256 = vector_view<256, T>;
