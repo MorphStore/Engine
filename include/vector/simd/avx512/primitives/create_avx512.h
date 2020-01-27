cc3c13ced1912d21ba80d6d76d3f8faeab4528ef
DerSchmidt <Lennart.Schmidt.1995@gmail.com>
2020-01-27 11:57:07 +0100

    Hopefully a fix for avx512 set8 compiler Error

--- include/vector/simd/avx512/primitives/create_avx512.h
+++ include/vector/simd/avx512/primitives/create_avx512.h
@@ -156,9 +156,15 @@ create_simd += 1;
 create_simd += 1;
 #endif
          trace( "[VECTOR] - set sse register." );
-         return _mm512_set_epi8(a63, a62, a61, a60, a59, a58, a57, a56, a55, a54, a53, a52, a51, a50, a49, a48, a47, a46, a45, a44, a43, a42, a41, a40,
-            a39, a38, a37, a36, a35, a34, a33, a32, a31, a30, a29, a28, a27, a26, a25, a24, a23, a22, a21, a20, a19, a18, a17, a16,
-            a15, a14, a13, a12, a11, a10, a9, a8,a7, a6, a5, a4, a3, a2, a1, a0);
+
+
+         __m512i v1 =_mm512_set_epi16(a63, a61, a59, a57, a55, a53, a51, a49, a47, a45, a43, a41, a39, a37, a35, a33, a31, a29, a27, a25, a23, a21, a19, a17, a15, a13, a11, a9, a7, a5, a3, a1);
+         __m512i v2 = _mm512_set_epi16(a62, a60, a58, a56, a54, a52, a50, a48, a46, a44, a42, a40, a38, a36, a34, a32, a30, a28, a26, a24, a22, a20, a18, a16, a14, a12, a10, a8, a6, a4, a2, a0);
+         __m512i and _mm512_set1_epi16(0xFF);
+         return _mm512_or_si512(
+               _mm512_sll_epi16(_mm512_and_si512(v1, and), 8),
+               _mm512_and_si512(v1, and)
+            );
       }
 
       template< typename U = T, typename std::enable_if< std::is_integral< U >::value, int >::type = 0 >
@@ -169,9 +175,14 @@ create_simd += 1;
 create_simd += 1;
 #endif
          trace( "[VECTOR] - set_sequence sse register." );
-         return _mm512_set_epi8(a+63*b,a+62*b,a+61*b,a+60*b,a+59*b,a+58*b,a+57*b,a+56*b,a+55*b,a+54*b,a+53*b,a+52*b,a+51*b,a+50*b,
-            a+49*b, a+48*b,a+47*b,a+46*b,a+45*b,a+44*b,a+43*b,a+42*b,a+41*b,a+40*b,a+39*b,a+38*b,a+37*b,a+36*b,a+35*b, a+34*b,a+33*b,a+32*b,a+31*b,a+30*b,a+29*b,a+28*b,a+27*b,a+26*b,a+25*b,a+24*b,a+23*b,a+22*b,a+21*b,a+20*b,
-            a+19*b, a+18*b,a+17*b,a+16*b,a+15*b,a+14*b,a+13*b,a+12*b,a+11*b,a+10*b,a+9*b,a+8*b,a+7*b,a+6*b,a+5*b, a+4*b,a+3*b,a+2*b,a+b, a);
+         __m512i v1 = _mm512_set_epi16(a+63*b, a+61*b, a+59*b, a+57*b, a+55*b, a+53*b, a+51*b, a+49*b, a+47*b, a+45*b, a+43*b, a+41*b, a+39*b, a+37*b, a+35*b, a+33*b, a+31*b, a+29*b, a+27*b, a+25*b, a+23*b, a+21*b, a+19*b, a+17*b, a+15*b, a+13*b, a+11*b, a+9*b, a+7*b, a+5*b, a+3*b, a+b);
+         __m512i v2 = _mm512_set_epi16(a+62*b, a+60*b, a+58*b, a+56*b, a+54*b, a+52*b, a+50*b, a+48*b, a+46*b, a+44*b, a+42*b, a+40*b, a+38*b, a+36*b,  a+34*b, a+32*b, a+30*b, a+28*b, a+26*b, a+24*b, a+22*b, a+20*b, a+18*b, a+16*b, a+14*b, a+12*b, a+10*b, a+8*b, a+6*b, a+4*b, a+2*b, a);
+
+         __m512i and _mm512_set1_epi16(0xFF);
+         return _mm512_or_si512(
+               _mm512_sll_epi16(_mm512_and_si512(v1, and), 8),
+               _mm512_and_si512(v1, and)
+            );
       }
 
       template< typename U = T, typename std::enable_if< std::is_integral< U >::value, int >::type = 0 >
