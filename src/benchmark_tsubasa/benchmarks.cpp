34dca35b4058a093849cfb23e5ed4b5e65a9220b
Wilhelm Mundt <Wilhelm.Mundt@mailbox.tu-dresden.de>
2020-04-18 11:20:44 +0200

    added documentation for scalable vector lengths

--- src/benchmark_tsubasa/benchmarks.cpp
+++ src/benchmark_tsubasa/benchmarks.cpp
@@ -1,3 +1,7 @@
+/* Benchmark application to benchmark performance impact of scalable vector lengths on certain operators.
+*
+*
+*/
 #include <iostream>
 
 #include "benchmark_agg_sum.h"
@@ -5,15 +9,13 @@
 
 int main( void ) {
 
-    // ************************************************************************
-    // * Query execution
-    // ************************************************************************
     using ve = tsubasa<v16384<uint64_t> >;
 
    
     std::cout << "Query execution started... ";
     std::cout.flush();
 
+// files that contain benchmark results
     std::string filename1 = "agg_sum_clean";
     std::string filename2 = "project_clean";
 
