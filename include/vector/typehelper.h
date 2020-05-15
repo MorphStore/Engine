ebfc12c9cb786566565591e7418a1f11e5532cbd
Felicita Gernhardt <felicitagernha@gmail.com>
2020-05-15 11:30:59 +0200

    added auxiliary functions

---
+++ include/vector/typehelper.h
@@ -0,0 +1,21 @@
+
+using namespace morphstore;
+using namespace vectorlib;
+
+template <class S, class T>
+class TypeHelper
+{
+};
+
+
+template <template <typename> class T1,
+          template <uint16_t, typename> class T2,
+          uint16_t N,
+          class T3,
+          class T4>
+class TypeHelper<T1<T2<N, T3> >, T4>
+{
+public:
+    typedef T1<T2<N, T4> > newbasetype;
+};
+
