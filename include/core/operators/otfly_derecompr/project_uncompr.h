37b13a49d7bcb4f91ee3251a98f84477bb152021
Alexander Krause <alexander.krause@tu-dresden.de>
2020-09-18 13:24:58 +0200

    Updated Project operator

--- include/core/operators/otfly_derecompr/project_uncompr.h
+++ include/core/operators/otfly_derecompr/project_uncompr.h
@@ -1,9 +1,8 @@
 //
 // Created by jpietrzyk on 26.04.19.
 //
-
-#ifndef MORPHSTORE_CORE_OPERATORS_GENERAL_VECTORIZED_PROJECT_UNCOMPR_H
-#define MORPHSTORE_CORE_OPERATORS_GENERAL_VECTORIZED_PROJECT_UNCOMPR_H
+#ifndef MORPHSTORE_CORE_OPERATORS_OTFLY_DERECOMPR_UNCOMPR_H
+#define MORPHSTORE_CORE_OPERATORS_OTFLY_DERECOMPR_UNCOMPR_H
 
 #include <core/utils/preprocessor.h>
 
@@ -12,23 +11,23 @@
 
 namespace morphstore {
 
-       using namespace vectorlib;
-       
-   template<class VectorExtension>
+    using namespace vectorlib;
+
+   template<class t_vector_extension>
    struct project_t_processing_unit {
-      IMPORT_VECTOR_BOILER_PLATE(VectorExtension)
+      IMPORT_VECTOR_BOILER_PLATE(t_vector_extension)
       MSV_CXX_ATTRIBUTE_FORCE_INLINE static vector_t apply(
          base_t const * p_DataPtr,
          vector_t p_PosVector
       ) {
          //@todo: Is it better to avoid gather here?
-         return (vectorlib::gather<VectorExtension, vector_size_bit::value, sizeof(base_t)>(p_DataPtr, p_PosVector));
+         return (vectorlib::gather<t_vector_extension, vector_size_bit::value, sizeof(base_t)>(p_DataPtr, p_PosVector));
       }
    };
 
-   template<class VectorExtension>
+   template<class t_vector_extension>
    struct project_t_batch {
-      IMPORT_VECTOR_BOILER_PLATE(VectorExtension)
+      IMPORT_VECTOR_BOILER_PLATE(t_vector_extension)
       MSV_CXX_ATTRIBUTE_FORCE_INLINE static void apply(
          base_t const *& p_DataPtr,
          base_t const *& p_PosPtr,
@@ -36,27 +35,27 @@ namespace morphstore {
          size_t const p_Count
       ) {
          for(size_t i = 0; i < p_Count; ++i) {
-            vector_t posVector = vectorlib::load<VectorExtension, vectorlib::iov::ALIGNED, vector_size_bit::value>(p_PosPtr);
+            vector_t posVector = vectorlib::load<t_vector_extension, vectorlib::iov::ALIGNED, vector_size_bit::value>(p_PosPtr);
             vector_t lookupVector =
-               project_t_processing_unit<VectorExtension>::apply(
+               project_t_processing_unit<t_vector_extension>::apply(
                   p_DataPtr,
                   posVector
                );
-            vectorlib::store<VectorExtension, vectorlib::iov::ALIGNED, vector_size_bit::value>(p_OutPtr, lookupVector);
+            vectorlib::store<t_vector_extension, vectorlib::iov::ALIGNED, vector_size_bit::value>(p_OutPtr, lookupVector);
             p_PosPtr += vector_element_count::value;
             p_OutPtr += vector_element_count::value;
          }
       }
    };
 
-   template<class VectorExtension>
+   template<class t_vector_extension, class t_out_data_f, class t_in_data_f, class t_in_pos_f>
    struct project_t {
-      IMPORT_VECTOR_BOILER_PLATE(VectorExtension)
+      IMPORT_VECTOR_BOILER_PLATE(t_vector_extension)
       MSV_CXX_ATTRIBUTE_FORCE_INLINE static
-      column<uncompr_f> const *
+      column< t_out_data_f > const *
       apply(
-         column< uncompr_f > const * const p_DataColumn,
-         column< uncompr_f > const * const p_PosColumn
+         column< t_in_data_f > const * const p_DataColumn,
+         column< t_in_pos_f > const * const p_PosColumn
       ) {
          size_t const inPosCount = p_PosColumn->get_count_values();
          size_t const inUsedBytes = p_PosColumn->get_size_used_byte();
@@ -66,7 +65,8 @@ namespace morphstore {
          base_t const * inPosPtr = p_PosColumn->get_data( );
          auto outDataCol = new column<uncompr_f>(inUsedBytes);
          base_t * outDataPtr = outDataCol->get_data( );
-         project_t_batch<VectorExtension>::apply(inDataPtr, inPosPtr, outDataPtr, vectorCount);
+
+         project_t_batch<t_vector_extension>::apply(inDataPtr, inPosPtr, outDataPtr, vectorCount);
          project_t_batch<scalar<v64<uint64_t>>>::apply(inDataPtr, inPosPtr, outDataPtr, remainderCount);
 
          outDataCol->set_meta_data(inPosCount, inUsedBytes);
@@ -87,5 +87,5 @@ namespace morphstore {
 
 
 
-#endif //MORPHSTORE_CORE_OPERATORS_GENERAL_VECTORIZED_PROJECT_UNCOMPR_H
+#endif //MORPHSTORE_CORE_OPERATORS_OTFLY_DERECOMPR_UNCOMPR_H
 
