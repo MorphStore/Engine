6a5f9db4af19c8a71a2d9d93681d037fe8d76240
Eric Mier <eric.mier@tu-dresden.de>
2020-09-21 17:36:41 +0200

    Renamed agg_sum_uncompr.h to agg_sum_all.h

--- include/core/operators/uncompr/agg_sum_all.h
+++ include/core/operators/uncompr/agg_sum_all.h
@@ -1,9 +1,27 @@
-//
-// Created by jpietrzyk on 26.04.19.
-//
+/**********************************************************************************************
+ * Copyright (C) 2019 by MorphStore-Team                                                      *
+ *                                                                                            *
+ * This file is part of MorphStore - a compression aware vectorized column store.             *
+ *                                                                                            *
+ * This program is free software: you can redistribute it and/or modify it under the          *
+ * terms of the GNU General Public License as published by the Free Software Foundation,      *
+ * either version 3 of the License, or (at your option) any later version.                    *
+ *                                                                                            *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;  *
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  *
+ * See the GNU General Public License for more details.                                       *
+ *                                                                                            *
+ * You should have received a copy of the GNU General Public License along with this program. *
+ * If not, see <http://www.gnu.org/licenses/>.                                                *
+ **********************************************************************************************/
 
-#ifndef MORPHSTORE_CORE_OPERATORS_GENERAL_VECTORIZED_AGG_SUM_UNCOMPR_H
-#define MORPHSTORE_CORE_OPERATORS_GENERAL_VECTORIZED_AGG_SUM_UNCOMPR_H
+/**
+ * @file agg_sum.h
+ * @brief 
+ */
+
+#ifndef MORPHSTORE_CORE_OPERATORS_UNCOMPR_AGG_SUM_UNCOMPR_H
+#define MORPHSTORE_CORE_OPERATORS_UNCOMPR_AGG_SUM_UNCOMPR_H
 
 #include <core/operators/interfaces/agg_sum.h>
 
@@ -133,5 +151,5 @@ namespace morphstore {
 
 
 
-#endif //MORPHSTORE_CORE_OPERATORS_GENERAL_VECTORIZED_AGG_SUM_UNCOMPR_H
+#endif //MORPHSTORE_CORE_OPERATORS_UNCOMPR_AGG_SUM_UNCOMPR_H
 
