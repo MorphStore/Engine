514dd6fae3e9ad66fad010940d5e3636e71840c5
Eric Mier <eric.mier@tu-dresden.de>
2020-11-20 12:07:09 +0100

    Added documentation. Improved readability.

--- include/core/utils/helper_types.h
+++ include/core/utils/helper_types.h
@@ -17,47 +17,63 @@
 
 /**
  * @file helper_types.h
- * @brief Brief description
- * @todo TODOS?
+ * @brief This file contains a helper structure for using void pointers.
  */
 
 #ifndef MORPHSTORE_CORE_UTILS_HELPER_TYPES_H
 #define MORPHSTORE_CORE_UTILS_HELPER_TYPES_H
 
-#if !defined(MSV_NO_LOG) && defined(DEBUG)
+#if ! defined(MSV_NO_LOG) && defined(DEBUG)
 #  include <typeinfo>
 #endif
+
 #include <type_traits>
+
 namespace morphstore {
-
-   struct voidptr_helper {
-      void const * const m_Ptr;
-      constexpr voidptr_helper(void const * const p_Ptr): m_Ptr{p_Ptr}{}
-      template<typename T>
-      MSV_CXX_ATTRIBUTE_INLINE MSV_CXX_ATTRIBUTE_CONST
-      operator T const * () const {
-         trace("[ VoidPtr Helper ] Implicit cast [ const void * const ] --> [ const ", typeid(T).name(), " * ]");
-         return reinterpret_cast<T const * const >( m_Ptr );
-      }
-      template<typename T>
-      MSV_CXX_ATTRIBUTE_INLINE MSV_CXX_ATTRIBUTE_CONST
-      operator T * () const {
-         trace("[ VoidPtr Helper ] Implicit cast [ const void * const ] --> [ ", typeid(T).name(), " * ]");
-         return reinterpret_cast<T * >( const_cast< void * >( m_Ptr ) );
-      }
-   };
-   using voidptr_t = voidptr_helper;
-
-   template< int Alignment, typename T, size_t N >
-   struct alignas( Alignment ) aligned_array : public std::array< T, N >{
-      template< class... U >
-      aligned_array( U&&... u ) : std::array< T, N >{ std::forward< U >( u )... } {}
-   };
-
-#  define IMM_INT32(N) std::integral_constant< int32_t, N >()
-#  define IMM_UINT32(N) std::integral_constant< uint32_t, N >()
-#  define IMM_INT64(N) std::integral_constant< int64_t, N >()
-#  define IMM_UINT64(N) std::integral_constant< uint64_t, N >()
-
+    
+    /**
+     * Helper structure for storing void pointers. Enables implicit casting to other type pointers.
+     */
+    struct voidptr_helper {
+        /// the actual pointer
+        void const * const m_Ptr;
+        
+        /// Constructor of a void pointer. Takes a pointer to store.
+        constexpr voidptr_helper(void const * const p_Ptr) : m_Ptr{p_Ptr} {}
+        
+        /// Implicit pointer cast operator. Preserves const property.
+        template< typename T >
+        MSV_CXX_ATTRIBUTE_INLINE MSV_CXX_ATTRIBUTE_CONST
+        operator T const *() const {
+            trace("[ VoidPtr Helper ] Implicit cast [ const void * const ] --> [ const ", typeid(T).name(), " * ]");
+            return reinterpret_cast<T const * const >( m_Ptr );
+        }
+        
+        /// Implicit pointer cast operator. Removes const property.
+        template< typename T >
+        MSV_CXX_ATTRIBUTE_INLINE MSV_CXX_ATTRIBUTE_CONST
+        operator T *() const {
+            trace("[ VoidPtr Helper ] Implicit cast [ const void * const ] --> [ ", typeid(T).name(), " * ]");
+            return reinterpret_cast<T * >( const_cast< void * >( m_Ptr ));
+        }
+    };
+    
+    /// Helper structure for storing void pointers. Enables implicit casting to other type pointers.
+    using voidptr_t = voidptr_helper;
+    
+    /// @todo @JP Keine Ahnung was es tut. Bitte dokumentieren!
+    template< int Alignment, typename T, size_t N >
+    struct alignas( Alignment ) aligned_array : public std::array<T, N> {
+        template< class... U >
+        aligned_array(U && ... u) : std::array<T, N>{std::forward<U>(u)...} {}
+    };
+    
+    /// @todo @all useless. unnecessarily worsens code clarity. remove?
+    #define IMM_INT32(N) std::integral_constant< int32_t, N >()
+    #define IMM_UINT32(N) std::integral_constant< uint32_t, N >()
+    #define IMM_INT64(N) std::integral_constant< int64_t, N >()
+    #define IMM_UINT64(N) std::integral_constant< uint64_t, N >()
+    
 }
+
 #endif //MORPHSTORE_CORE_UTILS_BASIC_TYPES_H
