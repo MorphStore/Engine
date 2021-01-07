0d2f7efb0d1bbbcd9b6fc18942db2533fefefb46
Eric Mier <eric.mier@tu-dresden.de>
2021-01-07 10:03:03 +0100

    Reformat file and more comments

--- include/core/storage/column.h
+++ include/core/storage/column.h
@@ -17,7 +17,7 @@
 
 /**
  * @file column.h
- * @brief Brief description
+ * @brief This file implements the column, the key component to store data in morph store.
  * @todo Which headers should be included to use the memory manager here?
  */
 
@@ -35,119 +35,143 @@
 
 #include <cstring>
 
+#include <forward>
+#include "Storage.h"
+
 namespace morphstore {
-
-enum class storage_persistence_type {
-   globalScope,
-   queryScope
-};
-
-template< class F >
-class column {
-   static_assert(
-      std::is_base_of< format, F >::value,
-      "column: template parameter F must be a subclass of format"
-   );
     
-   public:
-      // Creates an emphemeral column. Intended for intermediate results.
-      column( size_t p_SizeAllocatedByte ) : column(
-         storage_persistence_type::queryScope,
-         p_SizeAllocatedByte
-      ) {
-         //
-      };
-         
-      column( column< F > const & ) = delete;
-      column( column< F > && ) = delete;
-      column< F > & operator= ( column< F > const & ) = delete;
-      column< F > & operator= ( column< F > && ) = delete;
-
-      virtual ~column( ) {
-#ifdef MSV_NO_SELFMANAGED_MEMORY
-         free( m_DataUnaligned );
-#else
-         if( m_PersistenceType == storage_persistence_type::globalScope ) {
-            general_memory_manager::get_instance( ).deallocate( m_Data );
-         }
-#endif
-      }
+    /**
+     * @todo @all Describe what this enum does.
+     */
+    enum class storage_persistence_type {
+        globalScope,
+        queryScope
+    };
+    
+    /**
+     * Key datastructure to store data in morphstore.
+     * @tparam TFormat Compression format of this column (uncompr_f, delta_f, dynamic_vbp_f, empty_f, for_f, k_wise_ns_f, static_vbp_f, ...?)
+     */
+    template< IFormat TFormat >
+    class column : public Storage {
+        /// make Partitioned Column friend to this class
+//        template<IFormat TFormat>
+//        template<>
+        template<IFormat, IPartitioner>
+        friend class PartitionedColumn;
+//        static_assert(
+//          std::is_base_of<format, TFormat>::value,
+//          "column: template parameter F must be a subclass of format"
+//        );
       
-   private:
-      column_meta_data m_MetaData;
-#ifdef MSV_NO_SELFMANAGED_MEMORY
-      void * m_DataUnaligned;
-#endif
-      voidptr_t m_Data;
-      // @todo Actually, the persistence type only makes sense if we use our
-      // own memory manager.
-      storage_persistence_type m_PersistenceType;
+      private:
+        /// Stores meta data of column (value count, bytes used, compressed bytes used).
+        column_meta_data m_MetaData;
+        
+        #ifdef MSV_NO_SELFMANAGED_MEMORY
+        /// Pointer to (possibly unaligned) allocated memory.
+        /// This pointer is used to create an aligned data pointer within the allocated memory.
+        void * m_DataUnaligned;
+        #endif
+        
+        /// (64bit) Aligned data pointer.
+        voidptr_t m_Data;
+        
+        /// @todo @all Actually, the persistence type only makes sense if we use our own memory manager.
+        storage_persistence_type m_PersistenceType;
+        
+        /// This flag indicates if the (compressed) column is already prepared for random access.
+        mutable bool m_IsPreparedForRndAccess;
+        
+        /// Constructor of column. When using morphstores own memory manager the column is either persistent or ephemeral.
+        column(storage_persistence_type p_PersistenceType, size_t p_SizeAllocatedByte) :
+            m_MetaData{0, 0, 0, p_SizeAllocatedByte},
+            m_PersistenceType{p_PersistenceType},
+            m_IsPreparedForRndAccess(false),
+            #ifdef MSV_NO_SELFMANAGED_MEMORY
+            m_DataUnaligned{malloc(get_size_with_alignment_padding(p_SizeAllocatedByte))},
+            m_Data{create_aligned_ptr(m_DataUnaligned)}
+            #else
+            m_Data{
+               ( p_PersistenceType == storage_persistence_type::globalScope )
+               ? general_memory_manager::get_instance( ).allocate( p_SizeAllocatedByte )
+               : malloc( p_SizeAllocatedByte )
+            }
+            #endif
+            { /* ... */ }
       
-      mutable bool m_IsPreparedForRndAccess;
+      public:
+        /// Creates an ephemeral column. Intended for intermediate results.
+        column(size_t p_SizeAllocatedByte) : column(storage_persistence_type::queryScope, p_SizeAllocatedByte)
+        { /* ... */};
+        
+        /// <b>Warning</b>: copy constructor is not supported
+        column(column<TFormat> const &) = delete;
+        /// <b>Warning</b>: move constructor is not supported
+        column(column<TFormat> &&) = delete;
+        
+        /// <b>Warning</b>: copy operator is not supported
+        column<TFormat> & operator =(column<TFormat> const &) = delete;
+        /// <b>Warning</b>: move operator is not supported
+        column<TFormat> & operator =(column<TFormat> &&) = delete;
+        
+        virtual ~column() {
+            #ifdef MSV_NO_SELFMANAGED_MEMORY
+            free(m_DataUnaligned);
+            #else
+            if( m_PersistenceType == storage_persistence_type::globalScope ) {
+               general_memory_manager::get_instance( ).deallocate( m_Data );
+            }
+            #endif
+        }
       
-      column(
-         storage_persistence_type p_PersistenceType,
-         size_t p_SizeAllocatedByte
-      ) :
-         m_MetaData{ 0, 0, 0, p_SizeAllocatedByte },
-#ifdef MSV_NO_SELFMANAGED_MEMORY
-         m_DataUnaligned{
-            malloc( get_size_with_alignment_padding( p_SizeAllocatedByte ) )
-         },
-         m_Data{ create_aligned_ptr( m_DataUnaligned ) },
-#else
-         m_Data{
-            ( p_PersistenceType == storage_persistence_type::globalScope )
-            ? general_memory_manager::get_instance( ).allocate( p_SizeAllocatedByte )
-            : malloc( p_SizeAllocatedByte )
-         },
-#endif
-         m_PersistenceType{ p_PersistenceType },
-         m_IsPreparedForRndAccess(false)
-      {
-         //
-      }
-
-   public:
-      inline voidptr_t get_data( void ) const {
-         return m_Data;
-      }
-      inline size_t get_count_values( void ) const {
-         return m_MetaData.m_CountLogicalValues;
-      }
-      inline void set_count_values( size_t p_CountValues ) {
-         m_MetaData.m_CountLogicalValues = p_CountValues;
-      }
-      inline size_t get_size_used_byte( void ) const {
-         return m_MetaData.m_SizeUsedByte;
-      }
-      inline void set_size_used_byte( size_t p_SizeUsedByte ) {
-         m_MetaData.m_SizeUsedByte = p_SizeUsedByte;
-      }
-      inline size_t get_size_compr_byte( void ) const {
-         return m_MetaData.m_SizeComprByte;
-      }
-      inline void set_size_compr_byte( size_t p_SizeComprByte ) {
-         m_MetaData.m_SizeComprByte = p_SizeComprByte;
-      }
-      inline void set_meta_data(
-         size_t p_CountValues,
-         size_t p_SizeUsedByte,
-         size_t p_SizeComprByte
-      )  {
-         m_MetaData.m_CountLogicalValues = p_CountValues;
-         m_MetaData.m_SizeUsedByte = p_SizeUsedByte;
-         m_MetaData.m_SizeComprByte = p_SizeComprByte;
-      }
-      inline void set_meta_data( size_t p_CountValues, size_t p_SizeUsedByte )  {
-          set_meta_data(p_CountValues, p_SizeUsedByte, 0);
-      }
-      
+        inline voidptr_t get_data() const {
+            return m_Data;
+        }
+        
+        inline size_t get_count_values() const {
+            return m_MetaData.m_CountLogicalValues;
+        }
+        
+        inline void set_count_values(size_t p_CountValues) {
+            m_MetaData.m_CountLogicalValues = p_CountValues;
+        }
+        
+        inline size_t get_size_used_byte() const {
+            return m_MetaData.m_SizeUsedByte;
+        }
+        
+        inline void set_size_used_byte(size_t p_SizeUsedByte) {
+            m_MetaData.m_SizeUsedByte = p_SizeUsedByte;
+        }
+        
+        inline size_t get_size_compr_byte() const {
+            return m_MetaData.m_SizeComprByte;
+        }
+        
+        inline void set_size_compr_byte(size_t p_SizeComprByte) {
+            m_MetaData.m_SizeComprByte = p_SizeComprByte;
+        }
+        
+        inline void set_meta_data(
+          size_t p_CountValues,
+          size_t p_SizeUsedByte,
+          size_t p_SizeComprByte
+        ) {
+            m_MetaData.m_CountLogicalValues = p_CountValues;
+            m_MetaData.m_SizeUsedByte = p_SizeUsedByte;
+            m_MetaData.m_SizeComprByte = p_SizeComprByte;
+        }
+        
+        inline void set_meta_data(size_t p_CountValues, size_t p_SizeUsedByte) {
+            set_meta_data(p_CountValues, p_SizeUsedByte, 0);
+        }
+        
         // The following utility functions for working with the subdivision of
         // a column into a compressed main part and an uncompressed rest part
         // can definitely be simplified. However, they work and should not
         // matter w.r.t. performance anyway.
-      
+        
         /**
          * @brief Returns a pointer to the start of the uncompressed rest part
          * of the column.
@@ -164,10 +188,11 @@ class column {
          * column.
          */
         inline const voidptr_t get_data_uncompr_start() const {
-            return voidptr_t(create_data_uncompr_start(
-                    static_cast<uint8_t *>(m_Data) +
-                    m_MetaData.m_SizeComprByte
-            ));
+            return voidptr_t(
+              create_data_uncompr_start(
+                static_cast<uint8_t *>(m_Data) +
+                m_MetaData.m_SizeComprByte
+              ));
         }
         
         /**
@@ -180,7 +205,7 @@ class column {
          * @return A pointer to the start of the uncompressed rest part.
          */
         inline static uint8_t * create_data_uncompr_start(uint8_t * p_ComprEnd) {
-            if(std::is_same<F, uncompr_f>::value)
+            if (std::is_same<TFormat, uncompr_f>::value)
                 return create_aligned_ptr(p_ComprEnd);
             else
                 // Between the compressed main part and the uncompressed rest
@@ -191,7 +216,7 @@ class column {
                 // can exceed the size of one block, due to the way the
                 // operators handle the scalar part of the uncompressed rest.
                 return create_aligned_ptr(
-                        p_ComprEnd + 2 * F::get_size_max_byte(F::m_BlockSize)
+                  p_ComprEnd + 2 * TFormat::get_size_max_byte(TFormat::m_BlockSize)
                 );
         }
         
@@ -204,13 +229,13 @@ class column {
          */
         inline size_t get_count_values_uncompr() const {
             return (m_MetaData.m_SizeComprByte == m_MetaData.m_SizeUsedByte)
-                    ? 0
-                    : convert_size<uint8_t, uint64_t>(
-                            m_MetaData.m_SizeUsedByte - (
-                                    static_cast<const uint8_t *>(get_data_uncompr_start()) -
-                                    static_cast<const uint8_t *>(m_Data.m_Ptr)
-                            )
-                    );
+                   ? 0
+                   : convert_size<uint8_t, uint64_t>(
+                m_MetaData.m_SizeUsedByte - (
+                  static_cast<const uint8_t *>(get_data_uncompr_start()) -
+                  static_cast<const uint8_t *>(m_Data.m_Ptr)
+                )
+              );
         }
         
         /**
@@ -238,54 +263,54 @@ class column {
          * 
          * @return `true` if action was taken, `false` otherwise.
          */
-        template<class t_vector_extension>
+        template< class t_vector_extension >
         bool prepare_for_random_access() const {
-            if(m_IsPreparedForRndAccess || std::is_same<F, uncompr_f>::value)
+            if (m_IsPreparedForRndAccess || std::is_same<TFormat, uncompr_f>::value)
                 return false;
             
             const size_t countLogUncompr = get_count_values_uncompr();
-            if(!countLogUncompr)
+            if (! countLogUncompr)
                 return false;
             
             const uint8_t * rest8 = get_data_uncompr_start();
             const size_t countLogUncomprRoundUp =
-                    round_up_to_multiple(countLogUncompr, F::m_BlockSize);
+              round_up_to_multiple(countLogUncompr, TFormat::m_BlockSize);
             memset(
-                    const_cast<uint8_t *>(rest8) + convert_size<uint64_t, uint8_t>(countLogUncompr),
-                    0,
-                    convert_size<uint64_t, uint8_t>(
-                            countLogUncomprRoundUp - countLogUncompr
-                    )
+              const_cast<uint8_t *>(rest8) + convert_size<uint64_t, uint8_t>(countLogUncompr),
+              0,
+              convert_size<uint64_t, uint8_t>(
+                countLogUncomprRoundUp - countLogUncompr
+              )
             );
             uint8_t * comprEnd =
-                    static_cast<uint8_t *>(m_Data) + m_MetaData.m_SizeComprByte;
-            morph_batch<t_vector_extension, F, uncompr_f>(
-                    rest8, comprEnd, countLogUncomprRoundUp
+              static_cast<uint8_t *>(m_Data) + m_MetaData.m_SizeComprByte;
+            morph_batch<t_vector_extension, TFormat, uncompr_f>(
+              rest8, comprEnd, countLogUncomprRoundUp
             );
             
             m_IsPreparedForRndAccess = true;
             return true;
         }
         
-      // Creates a global scoped column. Intended for base data.
-      static column< F > * create_global_column(size_t p_SizeAllocByte) {
-         return new
-            (
-#ifdef MSV_NO_SELFMANAGED_MEMORY
-               malloc( sizeof( column< F > ) )
-#else
-               general_memory_manager::get_instance( ).allocate(
-                  sizeof( column< F > )
-               )
-#endif
-            )
-            column(
-               storage_persistence_type::globalScope,
-               p_SizeAllocByte
+        // Creates a global scoped column. Intended for base data.
+        static column<TFormat> * create_global_column(size_t p_SizeAllocByte) {
+            return new
+              (
+                #ifdef MSV_NO_SELFMANAGED_MEMORY
+                malloc(sizeof(column<TFormat>))
+                #else
+                general_memory_manager::get_instance( ).allocate(
+                   sizeof( column< F > )
+                )
+                #endif
+              )
+              column(
+              storage_persistence_type::globalScope,
+              p_SizeAllocByte
             );
-      }
-};
-
-
+        }
+    };
+    
+    
 }
 #endif //MORPHSTORE_CORE_STORAGE_COLUMN_H
