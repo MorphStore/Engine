553f5dd2df0d7a9c607dc8ee9f3b5fbb9571e71f
Eric Mier <eric.mier@tu-dresden.de>
2021-01-07 10:19:24 +0100

    New virtual vector tests

---
+++ src/parallelization/virtual_vector_template_test.cpp
@@ -0,0 +1,44 @@
+/**********************************************************************************************
+ * Copyright (C) 2019 by MorphStore-Team                                                      *
+ *                                                                                            *
+ * This file is part of MorphStore - a compression aware vectorized column store.             *
+ *                                                                                            *
+ * This program is free software: you can redistribute it and/or modify it under the          *
+ * terms of the GNU General Public License as published by the Free Software Foundation,      *
+ * either version 3 of the License, or (at your option) any later version.                    *
+ *                                                                                            *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;  *
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  *
+ * See the GNU General Public License for more details.                                       *
+ *                                                                                            *
+ * You should have received a copy of the GNU General Public License along with this program. *
+ * If not, see <http://www.gnu.org/licenses/>.                                                *
+ **********************************************************************************************/
+ 
+#include <iostream>
+
+#include <core/utils/logger.h>
+/// primitives
+#include <vector/vector_extension_structs.h>
+#include <vector/vector_primitives.h>
+
+using namespace morphstore;
+using namespace vectorlib;
+
+
+int main() {
+
+    using baseVectorExtension = avx512<v512<int32_t>>;
+    
+    using vv_core     = vv<VectorBuilder<cpu_openmp, 1, seq, 1, baseVectorExtension> >;
+    using vv_batch    = vv<VectorBuilder<cpu_openmp, 1, seq, 1, vv_core            > >;
+    using vv_operator = vv<VectorBuilder<cpu_openmp, 1, seq, 1, vv_batch           > >;
+
+    
+    std::cout << "Base Type [Base]:     " << typeid(baseVectorExtension::base_t).name() << std::endl;
+    std::cout << "Base Type [Core]:     " << typeid(vv_core::base_t).name()             << std::endl;
+    std::cout << "Base Type [Batch]:    " << typeid(vv_batch::base_t).name()            << std::endl;
+    std::cout << "Base Type [Operator]: " << typeid(vv_operator::base_t).name()         << std::endl;
+    
+    
+}
