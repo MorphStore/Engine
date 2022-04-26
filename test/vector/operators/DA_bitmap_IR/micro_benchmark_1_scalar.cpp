d7c45c9de3b25b3878030caaf6b0f9b3f82ac292
pfeiffer-tim <timtim.pfeiffer@gmail.com>
2022-04-26 15:43:17 +0200

    changed micro_benchmark_1 in ms

--- test/vector/operators/DA_bitmap_IR/micro_benchmark_1_scalar.cpp
+++ test/vector/operators/DA_bitmap_IR/micro_benchmark_1_scalar.cpp
@@ -40,7 +40,7 @@
 #include <map>
 #include <fstream>
 
-#define TEST_DATA_COUNT 1000*1000
+#define TEST_DATA_COUNT 1000*100
 
 using namespace morphstore;
 using namespace vectorlib;
@@ -61,9 +61,9 @@ int main( void ) {
     //std::vector<double> selectivity = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
     std::vector<double> selectivity = {0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5,
                                        0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0}; // more fine-granular
-    // result maps: key = selectivity, value = pair of (execution time in microsec, memory in bytes)
-    std::map<double, std::pair<std::chrono::duration<float, std::micro>, size_t>> position_list_results;
-    std::map<double, std::pair<std::chrono::duration<float, std::micro>, size_t>> bitmap_results;
+    // result maps: key = selectivity, value = pair of (execution time in millisec., memory in bytes)
+    std::map<double, std::pair<std::chrono::duration<float, std::milli>, size_t>> position_list_results;
+    std::map<double, std::pair<std::chrono::duration<float, std::milli>, size_t>> bitmap_results;
 
     // number elements to flush cache in clear_cache:
     const size_t cacheElements = 10 * 1000 * 1000;
@@ -89,7 +89,7 @@ int main( void ) {
                 >(inCol, predicate);
 
         auto end = high_resolution_clock::now();
-        auto exec_time = duration_cast<microseconds>(end - start);
+        auto exec_time = duration_cast<milliseconds>(end - start);
         auto used_bytes = result_pl->get_size_used_byte();
 
         // store result in map
@@ -114,7 +114,7 @@ int main( void ) {
                 >(inCol, predicate);
 
         auto end = high_resolution_clock::now();
-        auto exec_time = duration_cast<microseconds>(end - start);
+        auto exec_time = duration_cast<milliseconds>(end - start);
         auto used_bytes = result_bm->get_size_used_byte();
 
         // store result in map
@@ -127,18 +127,18 @@ int main( void ) {
     mapStream.open("micro_benchmark_1_scalar_results.txt");
 
     mapStream << "Position-List: " << "\n";
-    std::map<double, std::pair<std::chrono::duration<float, std::micro>, size_t>>::iterator it_pl;
+    std::map<double, std::pair<std::chrono::duration<float, std::milli>, size_t>>::iterator it_pl;
     for(it_pl = position_list_results.begin(); it_pl != position_list_results.end(); it_pl++){
         mapStream << "select. = " << (*it_pl).first
-            << ": exec.(μs) = " << (*it_pl).second.first.count()
+            << ": exec.(ms) = " << (*it_pl).second.first.count()
             << ", memory(B) = " << (*it_pl).second.second
             << "\n";
     }
     mapStream << "Bitmap: " << "\n";
-    std::map<double, std::pair<std::chrono::duration<float, std::micro>, size_t>>::iterator it_bm;
+    std::map<double, std::pair<std::chrono::duration<float, std::milli>, size_t>>::iterator it_bm;
     for(it_bm = bitmap_results.begin(); it_bm != bitmap_results.end(); it_bm++){
         mapStream << "selectivity = " << (*it_bm).first
-                  << ": exec. (μs) = " << (*it_bm).second.first.count()
+                  << ": exec. (ms) = " << (*it_bm).second.first.count()
                   << ", memory (B) = " << (*it_bm).second.second
                   << "\n";
     }
