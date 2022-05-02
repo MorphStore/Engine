1ad9f1ac4762fb87415659fc40e701ff2a48cab2
pfeiffer-tim <timtim.pfeiffer@gmail.com>
2022-05-02 17:08:20 +0200

    microbenchmark_3: added more selectivity values (.05 steps)

--- src/DA_bitmap_IR/micro_benchmark_3_uniform_sorted_unique_scalar.cpp
+++ src/DA_bitmap_IR/micro_benchmark_3_uniform_sorted_unique_scalar.cpp
@@ -74,7 +74,8 @@ int main( void ) {
     using processingStyle = scalar<v64<uint64_t>>;
 
     // Generate for each selectivity unique, sorted, uniform distributed base data and execute transformation + measure
-    std::vector<double> selectivities = {0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
+    std::vector<double> selectivities = {0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5,
+                                         0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0};
 
     // hash map to store results: key = selectivity, value = pair of {exec. time (μs), memory footprint (B)}
     // two measurements: PL -> BM && BM -> PL
